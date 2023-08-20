e1d426ae74b2a86123ced2ad0f12dadd34bf7bcd
jshu-git <jasonshu@terpmail.umd.edu>
2023-08-20 09:20:37 -0400

    remove defaults

--- scripts/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SHARED_DIR="$HOME/git/configs"
-
-FILES=(".bashrc" ".aliases" ".vimrc" ".tmux.conf")
-for file in "${FILES[@]}"; do
-    cp "$HOME/$file" "$SHARED_DIR/$file"
-    echo "copied $HOME/$file to $SHARED_DIR"
-done
--- scripts/copy2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SHARED_DIR="$HOME/git/configs"
-
-FILES=(".bashrc" ".aliases" ".vimrc" ".tmux.conf")
-for file in "${FILES[@]}"; do
-    cp "$SHARED_DIR/$file" "$HOME/$file" 
-    echo "copied $SHARED_DIR/$file $HOME/$file"
-done
