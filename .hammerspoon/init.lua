f5b0bcbdf1dcd5b73d76afe005bd14545d346df0
jshu <jasonshu@terpmail.umd.edu>
2023-10-08 10:35:03 -0400

    yadm

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,13 @@
+hyper = {"ctrl", "cmd", "alt", "shift"}
+
+-- application switcher
+apps = {
+    {"V", "Visual Studio Code"},
+    {"A", "Arc"},
+    {"F", "Finder"},
+}
+for i, app in ipairs(apps) do
+    hs.hotkey.bind(hyper, app[1], function()
+        hs.application.launchOrFocus(app[2])
+    end)
+end
