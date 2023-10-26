fd5d1ad91bb8fafaecdfc021a31caf94ebf2681b
jshu <jasonshu@terpmail.umd.edu>
2023-10-25 22:28:12 -0400

    alacritty hs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -82,28 +82,28 @@ hs.hotkey.bind(hyper, 'right', function()
 end)
 
 -- application switcher
--- apps = {
+apps = {
     -- {'1', 'company.thebrowser.Browser'}, -- for some reason, only arc doesn't allow for hiding
     -- {'2', 'com.microsoft.VSCode'},
-    -- {'3', 'org.alacritty'},
+    {'A', 'org.alacritty'},
     -- {'F', 'com.apple.finder'},
--- }
--- for i, app in ipairs(apps) do
---     hs.hotkey.bind(hyper, app[1], function()
---         local appObj = hs.application.get(app[2])
+}
+for i, app in ipairs(apps) do
+    hs.hotkey.bind(hyper, app[1], function()
+        local appObj = hs.application.get(app[2])
 
---         if not appObj then
---             -- The app is not running, so launch it
---             hs.application.launchOrFocusByBundleID(app[2])
---         elseif appObj:isFrontmost() then
---             -- The app is running and in focus, so send it to the back
---             appObj:hide()
---         else
---             -- The app is running but not in focus, so focus it
---             hs.application.launchOrFocusByBundleID(app[2])
---         end
---     end)
--- end
+        if not appObj then
+            -- The app is not running, so launch it
+            hs.application.launchOrFocusByBundleID(app[2])
+        elseif appObj:isFrontmost() then
+            -- The app is running and in focus, so send it to the back
+            appObj:hide()
+        else
+            -- The app is running but not in focus, so focus it
+            hs.application.launchOrFocusByBundleID(app[2])
+        end
+    end)
+end
 
 -- show desktop
 hs.hotkey.bind(hyper, 'D', function()
