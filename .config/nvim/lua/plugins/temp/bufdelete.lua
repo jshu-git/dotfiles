7ad08a8ad8b35bfe345707e21c31abee1870d7ea
jshu <shumatt190@gmail.com>
2024-05-01 09:55:42 -0400

    nvim

--- .config/nvim/lua/plugins/temp/bufdelete.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"famiu/bufdelete.nvim",
-	config = function()
-		vim.keymap.set("n", "<leader>x", "<cmd>Bdelete<CR>", { desc = "Delete Buffer" })
-		vim.keymap.set("n", "<leader>X", "<cmd>Bdelete!<CR>", { desc = "Delete Buffer (Force)" })
-	end,
-}
