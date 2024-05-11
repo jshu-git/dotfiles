04160549e4eeabb0dfe26a5bd239bb573b37efcd
jshu <shumatt190@gmail.com>
2024-05-11 18:29:47 -0400

    nvim

--- .config/nvim/lua/plugins/ui/blame.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"FabijanZulj/blame.nvim",
-	config = function()
-		require("blame").setup()
-		vim.keymap.set("n", "<leader>tb", "<cmd>BlameToggle window<CR>", { desc = "Toggle Blame (Window)" })
-	end,
-}
--- .config/nvim/lua/plugins/ui/no-clown-fiesta.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"aktersnurra/no-clown-fiesta.nvim",
-	priority = 1000,
-	config = function()
-		require("no-clown-fiesta").setup({
-			-- transparent = true,
-		})
-	end,
-}
