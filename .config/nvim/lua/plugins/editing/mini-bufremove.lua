007b9355965be904e463901fada32def9061f4ce
jshu <shumatt190@gmail.com>
2024-05-18 11:57:29 -0400

    nvim

--- .config/nvim/lua/plugins/editing/mini-bufremove.lua
+++ .config/nvim/lua/plugins/editing/mini-bufremove.lua
@@ -5,7 +5,6 @@ return {
 		config = function()
 			local mini = require("mini.bufremove")
 			mini.setup()
-
 			vim.keymap.set("n", "<leader>x", function()
 				mini.delete(0, false)
 			end, { desc = "Delete Buffer" })
