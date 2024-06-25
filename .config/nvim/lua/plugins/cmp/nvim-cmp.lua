ec421c37aeb3af80e8fdb8e27efda708d839ed84
jshu <jshu@jshus-air.lan>
2024-06-24 21:33:12 -0400

    cmp

--- .config/nvim/lua/plugins/cmp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/cmp/nvim-cmp.lua
@@ -4,13 +4,14 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		-- "hrsh7th/cmp-nvim-lsp-signature-help",
-		"hrsh7th/cmp-path",
+		-- "hrsh7th/cmp-path",
+		"FelipeLema/cmp-async-path",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/cmp-calc",
 		"dmitmel/cmp-cmdline-history",
-		-- rust
-		"ryo33/nvim-cmp-rust",
+		-- ui
+		"onsails/lspkind-nvim",
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -21,64 +22,76 @@ return {
 				-- { name = "nvim_lsp_signature_help" },
 				{ name = "buffer", max_item_count = 3 },
 				{ name = "calc" },
-				{ name = "path" },
+				{ name = "async_path" },
 			},
 			snippet = {
 				expand = function(args)
 					vim.snippet.expand(args.body)
 				end,
 			},
+			view = {
+				docs = {
+					auto_open = false,
+				},
+			},
 			window = {
 				completion = {
 					border = "single",
 					side_padding = 0,
 					scrollbar = false,
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
 				},
 				documentation = {
 					border = "single",
-					max_width = math.floor(0.33 * vim.o.columns),
-					max_height = math.floor(0.33 * vim.o.lines),
+					winhighlight = "FloatBorder:FloatBorder",
 				},
 			},
 			-- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
 			-- https://github.com/Roundlay/nvim/blob/main/lua/plugins/lazy-cmp.lua
+			-- formatting = {
+			-- 	fields = { "abbr", "menu", "kind" },
+			-- 	format = function(entry, item)
+			-- 		local menu_icon = {
+			-- 			nvim_lsp = "LSP",
+			-- 			buffer = "BUF",
+			-- 			async_path = "PTH",
+			-- 			cmdline = "CMD",
+			-- 			cmdline_history = "HST",
+			-- 			calc = "CLC",
+			-- 		}
+			-- 		item.menu = menu_icon[entry.source.name]
+			-- 		local content = item.abbr
+			-- 		local win_width = vim.api.nvim_win_get_width(0)
+			-- 		local max_content_width = math.floor(win_width * 0.15)
+			-- 		if #content > max_content_width then
+			-- 			item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 1) .. "…"
+			-- 		else
+			-- 			item.abbr = content .. (" "):rep(max_content_width - #content)
+			-- 		end
+			-- 		return item
+			-- 	end,
+			-- },
 			formatting = {
-				fields = { "abbr", "menu", "kind" },
-				format = function(entry, item)
-					local menu_icon = {
+				format = require("lspkind").cmp_format({
+					mode = "symbol_text",
+					-- mode = "symbol",
+					maxwidth = function()
+						return math.floor(vim.api.nvim_win_get_width(0) * 0.15)
+					end,
+					ellipsis_char = "…",
+					-- show_labelDetails = true,
+					menu = {
 						nvim_lsp = "LSP",
-						nvim_lua = "LUA",
 						buffer = "BUF",
-						path = "PTH",
+						async_path = "PTH",
 						cmdline = "CMD",
 						cmdline_history = "HST",
 						calc = "CLC",
-					}
-					item.menu = menu_icon[entry.source.name]
-					local content = item.abbr
-					local win_width = vim.api.nvim_win_get_width(0)
-					local max_content_width = math.floor(win_width * 0.15)
-					if #content > max_content_width then
-						item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 1) .. "…"
-					else
-						item.abbr = content .. (" "):rep(max_content_width - #content)
-					end
-					return item
-				end,
+					},
+				}),
 			},
-			-- formatting = {
-			-- 	format = require("lspkind").cmp_format({
-			-- 		-- mode = "symbol_text",
-			-- 		-- mode = "symbol",
-			-- 		maxwidth = function()
-			-- 			return 20
-			-- 		end,
-			-- 		ellipsis_char = "…",
-			-- 		show_labelDetails = true,
-			-- 	}),
-			-- },
 			completion = {
-				completeopt = "menu,menuone,preview,noinsert",
+				completeopt = "menu,menuone,noinsert",
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
@@ -90,7 +103,7 @@ return {
 					else
 						cmp.complete()
 					end
-				end, { "i", "s" }),
+				end),
 				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				-- snippets
@@ -100,21 +113,28 @@ return {
 					else
 						fallback()
 					end
-				end, { "i", "s" }),
+				end),
 				["<C-h>"] = cmp.mapping(function(fallback)
 					if vim.snippet.active({ direction = -1 }) then
 						vim.snippet.jump(-1)
 					else
 						fallback()
 					end
-				end, { "i", "s" }),
+				end),
+				["<C-g>"] = function()
+					if cmp.visible_docs() then
+						cmp.close_docs()
+					else
+						cmp.open_docs()
+					end
+				end,
 			}),
 		})
 
 		-- / cmdline setup
 		cmp.setup.cmdline({ "/", "?" }, {
 			completion = {
-				completeopt = "menu,menuone,preview,noinsert,noselect",
+				completeopt = "menu,menuone,noinsert,noselect",
 			},
 			mapping = cmp.mapping.preset.cmdline({
 				["<C-x>"] = cmp.mapping(function(fallback)
@@ -133,7 +153,7 @@ return {
 		-- : cmdline setup
 		cmp.setup.cmdline(":", {
 			completion = {
-				completeopt = "menu,menuone,preview,noinsert,noselect",
+				completeopt = "menu,menuone,noinsert,noselect",
 			},
 			mapping = cmp.mapping.preset.cmdline({
 				["<C-x>"] = cmp.mapping(function(fallback)
@@ -146,35 +166,9 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = "cmdline_history", max_item_count = 3 },
-				{ name = "cmdline" },
-				{ name = "path" },
+				{ name = "cmdline", max_item_count = 5 },
+				{ name = "async_path" },
 			}),
-		})
-
-		-- rust
-		local compare = require("cmp.config.compare")
-		cmp.setup.filetype({ "rust" }, {
-			sorting = {
-				priority_weight = 2,
-				comparators = {
-					-- deprioritize `.box`, `.mut`, etc.
-					require("cmp-rust").deprioritize_postfix,
-					-- deprioritize `Borrow::borrow` and `BorrowMut::borrow_mut`
-					require("cmp-rust").deprioritize_borrow,
-					-- deprioritize `Deref::deref` and `DerefMut::deref_mut`
-					require("cmp-rust").deprioritize_deref,
-					-- deprioritize `Into::into`, `Clone::clone`, etc.
-					require("cmp-rust").deprioritize_common_traits,
-					compare.offset,
-					compare.exact,
-					compare.score,
-					compare.recently_used,
-					compare.locality,
-					compare.sort_text,
-					compare.length,
-					compare.order,
-				},
-			},
 		})
 	end,
 }
