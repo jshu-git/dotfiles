6ce8e0aa45cc090823d68aebd2925760e8a75e61
jshu <jshu@jshus-air.lan>
2024-07-04 09:14:02 -0400

    nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,21 +11,4 @@ return {
       vim.cmd.colorscheme("arctic")
     end,
   },
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("rose-pine").setup({
-  --       styles = {
-  --         bold = false,
-  --         italic = false,
-  --         transparency = false,
-  --       },
-  --     })
-  --     -- vim.cmd("colorscheme rose-pine-main")
-  --     -- vim.cmd("colorscheme rose-pine-moon")
-  --   end,
-  -- },
 }
