5bac82162719b8935e50a06288485acd757dff5f
jshu <jshu@jshus-air.lan>
2024-07-09 19:19:22 -0400

    git

--- .config/nvim/lua/plugins/git/lazygit.lua
+++ .config/nvim/lua/plugins/git/lazygit.lua
@@ -9,7 +9,7 @@ return {
   },
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    { "<leader>gG", "<cmd>LazyGitConfig<cr>", desc = "LazyGit Config" },
+    { "<leader>gG", "<cmd>LazyGitConfig<cr>", desc = "LazyGit (Config)" },
   },
   config = function()
     vim.g.lazygit_floating_window_scaling_factor = 1
--- .config/nvim/lua/plugins/git/mini-git.lua
+++ .config/nvim/lua/plugins/git/mini-git.lua
@@ -3,6 +3,17 @@ return {
   config = function()
     local git = require("mini.git")
     git.setup()
-    -- vim.keymap.set("n", "<leader>gg", git.diff.get, { desc = "Git Diff" })
+    vim.keymap.set(
+      { "n", "x" },
+      "<leader>gs",
+      git.show_at_cursor,
+      { desc = "Git Show" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>gd",
+      git.show_diff_source,
+      { desc = "Git Diff" }
+    )
   end,
 }
