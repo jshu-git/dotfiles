4ee82d064b2cb10bcba92fdd89f4ac190b8ca882
jshu <jshu@jshus-air.lan>
2024-07-09 20:06:49 -0400

    git

--- .config/nvim/lua/plugins/git/mini-git.lua
+++ .config/nvim/lua/plugins/git/mini-git.lua
@@ -1,8 +1,16 @@
 return {
   "echasnovski/mini-git",
+  dependencies = {
+    "echasnovski/mini.extra",
+  },
   config = function()
     local git = require("mini.git")
-    git.setup()
+    local extra = require("mini.extra")
+    git.setup({
+      command = {
+        split = "vertical",
+      },
+    })
 
     vim.keymap.set({ "n", "x" }, "<leader>gs", function()
       if vim.bo.filetype ~= "git" then
@@ -10,6 +18,30 @@ return {
       else
         git.show_at_cursor({ split = "vertical" })
       end
-    end, { desc = "Git Show" })
+    end, { desc = "Show" })
+
+    vim.keymap.set(
+      { "n", "x" },
+      "<leader>gb",
+      "<cmd>Git blame -- %<CR>",
+      { desc = "Blame" }
+    )
+
+    -- pickers
+    -- vim.keymap.set(
+    --   "n",
+    --   "<leader>gf",
+    --   extra.pickers.git_files,
+    --   { desc = "Git Files (Tracked)" }
+    -- )
+    -- vim.keymap.set("n", "<leader>gm", function()
+    --   extra.pickers.git_files({ scope = "modified" })
+    -- end, { desc = "Git Files (Modified)" })
+    vim.keymap.set(
+      "n",
+      "<leader>gc",
+      extra.pickers.git_commits,
+      { desc = "Commits" }
+    )
   end,
 }
