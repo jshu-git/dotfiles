df01dd79ccae968f686a928956229abb7ef73dec
jshu <jasonshu@terpmail.umd.edu>
2024-07-11 14:29:04 -0400

    scrollview

---
+++ .config/nvim/lua/plugins/ui/scrollview.lua
@@ -0,0 +1,9 @@
+return {
+  "dstein64/nvim-scrollview",
+  config = function()
+    require("scrollview").setup({
+      signs_on_startup = { "search" },
+      search_symbol = "—",
+    })
+  end,
+}
