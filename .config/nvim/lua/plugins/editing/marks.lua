1670af818de02051311e1dad4dd18bea6cc1b94a
jshu <jasonshu@terpmail.umd.edu>
2024-07-12 15:53:46 -0400

    marks

--- .config/nvim/lua/plugins/editing/marks.lua
+++ .config/nvim/lua/plugins/editing/marks.lua
@@ -1,5 +1,8 @@
 return {
   "chentoast/marks.nvim",
+  dependencies = {
+    "echasnovski/mini.extra",
+  },
   config = function()
     require("marks").setup({
       default_mappings = false,
@@ -16,5 +19,14 @@ return {
       "<Plug>(Marks-deletebuf)",
       { desc = "Delete (Buffer)" }
     )
+
+    -- pickers
+    local extra = require("mini.extra")
+    vim.keymap.set("n", "mf", function()
+      extra.pickers.marks({ scope = "buf" })
+    end, { desc = "Marks (Buffer)" })
+    vim.keymap.set("n", "mF", function()
+      extra.pickers.marks({ scope = "global" })
+    end, { desc = "Marks (Global)" })
   end,
 }
