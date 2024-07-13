17ec6b16035479961b0e7ff6b631431c237b44a5
jshu <jasonshu@terpmail.umd.edu>
2024-07-13 15:34:50 -0400

    linediff

--- .config/nvim/lua/plugins/editing/linediff.lua
+++ .config/nvim/lua/plugins/editing/linediff.lua
@@ -1,11 +1,18 @@
 return {
   "AndrewRadev/linediff.vim",
   init = function()
+    vim.g.linediff_sign_highlight_group = "DiffChange"
+    vim.keymap.set(
+      "n",
+      "<leader>L",
+      "<cmd>Linediff<cr>",
+      { desc = "Diff Lines" }
+    )
     vim.keymap.set(
       "x",
-      "<leader>l",
+      "<leader>L",
       ":<C-U>'<,'>Linediff<cr>",
-      { desc = "Diff Line(s)" }
+      { desc = "Diff Lines (Visual)" }
     )
   end,
 }
