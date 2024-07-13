6bbad726617ef08bfa863a2d19b1b4d790ee8d6f
jshu <jasonshu@terpmail.umd.edu>
2024-07-13 13:36:54 -0400

    nord

---
+++ .config/nvim/lua/plugins/nord.lua
@@ -0,0 +1,15 @@
+return {
+  "gbprod/nord.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nord").setup({
+      diff = { mode = "fg" },
+      search = { theme = "vscode" },
+      styles = {
+        comments = { italic = false },
+      },
+    })
+    vim.cmd.colorscheme("nord")
+  end,
+}
