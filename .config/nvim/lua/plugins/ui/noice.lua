285b58c48868898ae8bc912592da6b3f425385d2
jshu <jasonshu@terpmail.umd.edu>
2024-07-13 00:00:00 -0400

    noice

---
+++ .config/nvim/lua/plugins/ui/noice.lua
@@ -0,0 +1,137 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    local noice = require("noice")
+    noice.setup({
+      cmdline = {
+        format = {
+          cmdline = false,
+          search_down = false,
+          search_up = false,
+          filter = false,
+          lua = false,
+          help = false,
+          input = false,
+        },
+      },
+      messages = {
+        view_history = "popup",
+      },
+      popupmenu = { enabled = false },
+      commands = {
+        history = { view = "popup" },
+        all = { view = "popup" },
+      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        signature = { enabled = false },
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
+      views = {
+        virtualtext = {
+          hl_group = "CodeiumSuggestion",
+        },
+        split = {
+          size = "50%",
+          close = {
+            keys = { "<esc>" },
+          },
+        },
+        popup = {
+          close = {
+            keys = { "<esc>" },
+          },
+          border = {
+            style = "single",
+          },
+          size = {
+            width = math.floor(0.75 * vim.o.columns),
+            height = math.floor(0.6 * vim.o.lines),
+          },
+        },
+        hover = {
+          scrollbar = false,
+          size = {
+            max_height = math.floor(0.25 * vim.o.lines),
+            max_width = math.floor(0.5 * vim.o.columns),
+          },
+          border = {
+            style = "single",
+            padding = { 0, 0 },
+          },
+        },
+        mini = {
+          timeout = 1000,
+          -- reverse = false,
+          -- position = {
+          --   row = 0,
+          --   col = "99%",
+          -- },
+          -- border = {
+          -- style = "single",
+          -- },
+        },
+        cmdline_popup = {
+          border = {
+            style = "single",
+            padding = { 0, 0 },
+          },
+          win_options = {
+            winhighlight = {
+              FloatBorder = "NoicePopupBorder",
+            },
+          },
+        },
+        confirm = {
+          position = {
+            row = "50%",
+            col = "50%",
+          },
+          border = {
+            style = "single",
+            padding = { 0, 0 },
+          },
+          win_options = {
+            winhighlight = {
+              FloatBorder = "NoicePopupBorder",
+            },
+          },
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>nn", function()
+      vim.cmd("messages")
+    end, { desc = "Messages" })
+    vim.keymap.set("n", "<leader>nN", function()
+      noice.cmd("Noice")
+    end, { desc = "Messages (Noice)" })
+    vim.keymap.set("n", "<leader>nl", function()
+      noice.cmd("last")
+    end, { desc = "Last" })
+    vim.keymap.set("n", "<leader>nd", function()
+      noice.cmd("errors")
+    end, { desc = "Errors" })
+
+    -- toggle
+    vim.keymap.set("n", "<leader>tt", function()
+      noice.cmd("dismiss")
+    end, { desc = "Noice (Clear)" })
+    vim.keymap.set("n", "<leader>tn", function()
+      noice.cmd("disable")
+    end, { desc = "Noice (Disable)" })
+    vim.keymap.set("n", "<leader>tN", function()
+      noice.cmd("enable")
+    end, { desc = "Noice (Enable)" })
+  end,
+}
