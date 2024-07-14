b5e7f054e5034cfc10e9685b332fad8dfedc2b5e
jshu <jasonshu@terpmail.umd.edu>
2024-07-14 16:26:28 -0400

    mini icons

--- .config/nvim/lua/plugins/ui/mini-icons.lua
+++ .config/nvim/lua/plugins/ui/mini-icons.lua
@@ -2,7 +2,12 @@ return {
   "echasnovski/mini.icons",
   config = function()
     local icons = require("mini.icons")
-    icons.setup()
+    icons.setup({
+      filetype = {
+        minifiles = { glyph = "󰉋" },
+        minipick = { glyph = "󰍉" },
+      },
+    })
     icons.mock_nvim_web_devicons()
   end,
 }
