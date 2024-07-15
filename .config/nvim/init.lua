bad1840479e91570b144981873cea4750f20d4e8
jshu <jasonshu@terpmail.umd.edu>
2024-07-14 21:07:16 -0400

    clean

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,18 @@
 if vim.env.SSH_CLIENT ~= nil then
   vim.g.editorconfig = false
 end
-require("config.options")
-require("config.autocommands")
-require("config.keymaps")
-require("lazy-nvim")
+
+-- https://nanotipsforvim.prose.sh/using-pcall-to-make-your-config-more-stable
+local function safe_require(module)
+  local success, errMsg = pcall(require, module)
+  if not success then
+    local msg = ("Error loading %q: %s"):format(module, errMsg)
+    vim.defer_fn(function()
+      vim.notify(msg, vim.log.levels.ERROR)
+    end, 1000)
+  end
+end
+safe_require("config.options")
+safe_require("config.autocommands")
+safe_require("config.keymaps")
+safe_require("lazy-nvim")
