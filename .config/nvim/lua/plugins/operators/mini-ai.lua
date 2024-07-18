5290c417e930447840ddd80b3edb915cb3863000
jshu <jasonshu@terpmail.umd.edu>
2024-07-18 18:15:27 -0400

    mini ai

--- .config/nvim/lua/plugins/operators/mini-ai.lua
+++ .config/nvim/lua/plugins/operators/mini-ai.lua
@@ -7,15 +7,17 @@ return {
     local gen_ai_spec = require("mini.extra").gen_ai_spec
     require("mini.ai").setup({
       custom_textobjects = {
-        B = gen_ai_spec.buffer(),
+        g = gen_ai_spec.buffer(),
         L = gen_ai_spec.line(),
         N = gen_ai_spec.number(),
+        -- LazyVim
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+        d = { "%f[%d]%d+" },
       },
     })
     vim.keymap.set("n", "<S-CR>", "ciq", { remap = true })
     vim.keymap.set("n", "<C-CR>", "cib", { remap = true })
     vim.keymap.set("x", "v", "iLo", { remap = true })
     vim.keymap.set("n", "yY", "yaL", { remap = true })
-    vim.keymap.set("n", "gG", "vaB", { desc = "Select All", remap = true })
   end,
 }
