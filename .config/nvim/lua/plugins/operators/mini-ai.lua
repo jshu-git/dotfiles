d676aad96933205875f35fc3228fd7fbc734b0b0
jshu <jasonshu@terpmail.umd.edu>
2024-07-19 19:56:36 -0400

    mini ai

--- .config/nvim/lua/plugins/operators/mini-ai.lua
+++ .config/nvim/lua/plugins/operators/mini-ai.lua
@@ -16,8 +16,6 @@ return {
       },
     })
     vim.keymap.set("n", "<S-CR>", "ciq", { remap = true })
-    vim.keymap.set("n", "<C-CR>", "cib", { remap = true })
     vim.keymap.set("x", "v", "iLo", { remap = true })
-    vim.keymap.set("n", "yY", "yaL", { remap = true })
   end,
 }
