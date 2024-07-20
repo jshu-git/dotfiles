5993a344a098087451adbbbc4c52d000c896ac99
jshu <jasonshu@terpmail.umd.edu>
2024-07-19 22:53:58 -0400

    ai

--- .config/nvim/lua/plugins/operators/mini-ai.lua
+++ .config/nvim/lua/plugins/operators/mini-ai.lua
@@ -6,16 +6,26 @@ return {
   config = function()
     local gen_ai_spec = require("mini.extra").gen_ai_spec
     require("mini.ai").setup({
+      mappings = {
+        around_next = "",
+        inside_next = "",
+        around_last = "",
+        inside_last = "",
+        goto_left = "",
+        goto_right = "",
+      },
       custom_textobjects = {
         g = gen_ai_spec.buffer(),
-        L = gen_ai_spec.line(),
-        N = gen_ai_spec.number(),
+        l = gen_ai_spec.line(),
+        n = gen_ai_spec.number(),
         -- LazyVim
         t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
         d = { "%f[%d]%d+" },
       },
     })
     vim.keymap.set("n", "<S-CR>", "ciq", { remap = true })
-    vim.keymap.set("x", "v", "iLo", { remap = true })
+    vim.keymap.set("x", "v", "ilo", { remap = true })
+    -- vim.keymap.set("n", "vil", "vilo", { remap = true })
+    -- vim.keymap.set("n", "val", "valo", { remap = true })
   end,
 }
