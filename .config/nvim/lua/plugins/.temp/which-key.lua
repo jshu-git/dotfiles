6da7471e16f840a5b91e8c62ae9a248de526e38c
jshu <jasonshu@terpmail.umd.edu>
2024-07-19 22:27:30 -0400

    clue

--- .config/nvim/lua/plugins/.temp/which-key.lua
+++ .config/nvim/lua/plugins/.temp/which-key.lua
@@ -1,71 +1,43 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter",
-	config = function()
-		local wk = require("which-key")
-		wk.setup({
-			plugins = {
-				marks = false,
-				presets = {
-					operators = false,
-					motions = false,
-					text_objects = false,
-				},
-			},
-			icons = {
-				separator = "",
-				group = "",
-			},
-			window = {
-				border = "single",
-				margin = { 0, 0, 0, 0 },
-				padding = { 0, 0, 0, 0 },
-			},
-			layout = {
-				spacing = 1,
-			},
-			triggers_blacklist = {
-				z = { "H" },
-			},
-		})
-		wk.register({
-			["<leader>f"] = { name = "Find…" },
-			["<leader>l"] = { name = "LSP…" },
-			["<leader>s"] = { name = "Session…" },
-			["<leader>t"] = { name = "Toggle…" },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  config = function()
+    local wk = require("which-key")
+    wk.setup({
+      preset = "helix",
+      delay = 250,
+      win = {
+        width = { max = 0.3 },
+        height = { max = -2 },
+        border = "single",
+        title_pos = "center",
+      },
+      sort = { "group", "desc" },
+      icons = {
+        breadcrumb = "󰐕",
+        separator = "│",
+        group = "",
+        mappings = false,
+      },
+      show_help = false,
+    })
 
-			-- disable some z mappings
-			["zC"] = "which_key_ignore",
-			["zc"] = "which_key_ignore",
-			["zR"] = "which_key_ignore",
-			["zr"] = "which_key_ignore",
-			["zM"] = "which_key_ignore",
-			["zm"] = "which_key_ignore",
-			["zO"] = "which_key_ignore",
-			["zo"] = "which_key_ignore",
-			["z<CR>"] = "which_key_ignore",
-			["zH"] = "which_key_ignore",
-			["zL"] = "which_key_ignore",
-			["ze"] = "which_key_ignore",
-			["zb"] = "which_key_ignore",
-			["zs"] = "which_key_ignore",
-			["zt"] = "which_key_ignore",
-			["zv"] = "which_key_ignore",
-			["zw"] = "which_key_ignore",
-			["zg"] = "which_key_ignore",
-
-			-- disable some g mappings
-			["g%"] = "which_key_ignore",
-			["gc"] = "which_key_ignore",
-			["gn"] = "which_key_ignore",
-			["gN"] = "which_key_ignore",
-			["gt"] = "which_key_ignore",
-			["gT"] = "which_key_ignore",
-
-			-- disable some <C-w> mappings
-			["<C-w><C-D>"] = "which_key_ignore",
-			["<C-w>d"] = "which_key_ignore",
-			["<C-w>T"] = "which_key_ignore",
-		})
-	end,
+    -- groups
+    wk.add({
+      {
+        mode = { "n" },
+        { "<leader>f", group = "Find" },
+        { "<leader>l", group = "LSP" },
+        { "<leader>t", group = "Toggle" },
+        { "<leader>g", group = "Git" },
+        { "<leader>s", group = "Session" },
+        { "<leader>n", group = "Noice" },
+        { "<leader>n", group = "Noice" },
+      },
+      {
+        mode = { "x" },
+        { "<leader>g", group = "Git" },
+      },
+    })
+  end,
 }
