7bbb284ed42325d44363b387697eb7d4344390e2
jshu <jasonshu@terpmail.umd.edu>
2024-07-19 22:57:51 -0400

    comment

--- .config/nvim/lua/plugins/editing/comment.lua
+++ .config/nvim/lua/plugins/editing/comment.lua
@@ -2,12 +2,14 @@ return {
   "numToStr/Comment.nvim",
   config = function()
     require("Comment").setup({
-      toggler = {
-        line = "<C-c>",
-        block = "<C-S-c>",
+      mappings = {
+        basic = false,
+        extra = false,
       },
     })
 
+    vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)")
+    vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
     vim.keymap.set("x", "<C-c>", "<Plug>(comment_toggle_linewise_visual)")
     vim.keymap.set("x", "<C-S-c>", "<Plug>(comment_toggle_blockwise_visual)")
     vim.keymap.set({ "x", "o" }, "ac", require("vim._comment").textobject)
