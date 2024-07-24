b3452899e946dbd2a0fe5df089dd5e690fa5da47
jshu <jasonshu@terpmail.umd.edu>
2024-07-24 15:55:36 -0400

    noice

--- .config/nvim/lua/plugins/ui/noice.lua
+++ .config/nvim/lua/plugins/ui/noice.lua
@@ -29,11 +29,11 @@ return {
         },
       },
       lsp = {
-        -- override = {
-        -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        -- ["vim.lsp.util.stylize_markdown"] = true,
-        -- ["cmp.entry.get_documentation"] = true,
-        -- },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
         signature = {
           auto_open = { enabled = false },
         },
