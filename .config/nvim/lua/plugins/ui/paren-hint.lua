1bf9aea4dc8c195b0edb2f0f7e871dd2061c2d84
jshu <jasonshu@terpmail.umd.edu>
2024-07-24 18:52:03 -0400

    paren-hint

---
+++ .config/nvim/lua/plugins/ui/paren-hint.lua
@@ -0,0 +1,10 @@
+return {
+  "briangwaltney/paren-hint.nvim",
+  config = function()
+    require("paren-hint").setup({
+      highlight = "NonText",
+      excluded_filetypes = {},
+      excluded_buftypes = {},
+    })
+  end,
+}
