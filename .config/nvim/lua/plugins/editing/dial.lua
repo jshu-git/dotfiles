dfcd0272ed3dd0f852dae57f5d83c772612ae800
jshu <jasonshu@terpmail.umd.edu>
2024-07-24 15:18:13 -0400

    dial

--- .config/nvim/lua/plugins/editing/dial.lua
+++ .config/nvim/lua/plugins/editing/dial.lua
@@ -1,6 +1,6 @@
 return {
   "monaqa/dial.nvim",
-  keys = { "-", "+" },
+  keys = { "-", "=" },
   config = function()
     local augend = require("dial.augend")
     require("dial.config").augends:register_group({
