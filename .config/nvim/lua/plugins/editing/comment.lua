c769abcdf3f5accb52a5eb441d2543e0e1df8789
jshu <jasonshu@terpmail.umd.edu>
2024-07-27 14:35:36 -0400

    comment

--- .config/nvim/lua/plugins/editing/comment.lua
+++ .config/nvim/lua/plugins/editing/comment.lua
@@ -1,5 +1,9 @@
 return {
   "numToStr/Comment.nvim",
+  keys = {
+    { "<C-c>", mode = { "n", "x" } },
+    { "<leader><C-c>", mode = { "n" } },
+  },
   config = function()
     require("Comment").setup({
       mappings = {
@@ -10,14 +14,21 @@ return {
 
     vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)")
     vim.keymap.set("x", "<C-c>", "<Plug>(comment_toggle_linewise_visual)")
+
+    -- block
+    vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
+    vim.keymap.set("x", "<C-S-c>", "<Plug>(comment_toggle_blockwise_visual)")
+
     vim.keymap.set(
       "n",
       "<leader><C-c>",
       "gcgc",
       { remap = true, desc = "Uncomment" }
     )
-    vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
-    vim.keymap.set("x", "<C-S-c>", "<Plug>(comment_toggle_blockwise_visual)")
-    vim.keymap.set("x", "agc", require("vim._comment").textobject)
+
+    -- textobject
+    -- for _, k in ipairs({ "a", "i" }) do
+    --   vim.keymap.set("x", k .. "gc", require("vim._comment").textobject)
+    -- end
   end,
 }
