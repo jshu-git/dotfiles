6b179ff59dc67b301f92699c7bba6d3082ce05e4
jshu <jasonshu@terpmail.umd.edu>
2024-07-26 23:05:32 -0400

    comment

--- .config/nvim/lua/plugins/editing/comment.lua
+++ .config/nvim/lua/plugins/editing/comment.lua
@@ -3,7 +3,7 @@ return {
   keys = {
     { "<C-c>", mode = { "n", "x" } },
     { "<C-S-c>", mode = { "n", "x" } },
-    { "ac", mode = { "x", "o" } },
+    { "aC", mode = { "x" } },
   },
   config = function()
     require("Comment").setup({
@@ -14,9 +14,10 @@ return {
     })
 
     vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)")
-    vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
     vim.keymap.set("x", "<C-c>", "<Plug>(comment_toggle_linewise_visual)")
+    vim.keymap.set("n", "<C-S-c>", "gcgc", { remap = true })
+    -- vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
     vim.keymap.set("x", "<C-S-c>", "<Plug>(comment_toggle_blockwise_visual)")
-    vim.keymap.set({ "x", "o" }, "ac", require("vim._comment").textobject)
+    vim.keymap.set("x", "aC", require("vim._comment").textobject)
   end,
 }
