e55b26e1da879a0043429ac3674ff082706bbd96
jshu <jasonshu@terpmail.umd.edu>
2024-07-26 23:05:35 -0400

    undotree

--- .config/nvim/lua/plugins/editing/undotree.lua
+++ .config/nvim/lua/plugins/editing/undotree.lua
@@ -6,7 +6,7 @@ return {
     vim.g.undotree_ShortIndicators = 1
     vim.g.undotree_DiffAutoOpen = 0
     vim.g.undotree_SetFocusWhenToggle = 1
-    vim.g.undotree_TreeNodeShape = "·"
+    vim.g.undotree_TreeNodeShape = "󰧞"
     vim.g.undotree_TreeReturnShape = "╲"
     vim.g.undotree_TreeVertShape = "│"
     vim.g.undotree_TreeSplitShape = "╱"
