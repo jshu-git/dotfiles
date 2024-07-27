2eedecb719e9ecf288c30b80adf1b026fec7f828
jshu <jasonshu@terpmail.umd.edu>
2024-07-26 23:13:09 -0400

    comment

--- .config/nvim/lua/plugins/editing/comment.lua
+++ .config/nvim/lua/plugins/editing/comment.lua
@@ -1,10 +1,5 @@
 return {
   "numToStr/Comment.nvim",
-  keys = {
-    { "<C-c>", mode = { "n", "x" } },
-    { "<C-S-c>", mode = { "n", "x" } },
-    { "aC", mode = { "x" } },
-  },
   config = function()
     require("Comment").setup({
       mappings = {
@@ -15,9 +10,14 @@ return {
 
     vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)")
     vim.keymap.set("x", "<C-c>", "<Plug>(comment_toggle_linewise_visual)")
-    vim.keymap.set("n", "<C-S-c>", "gcgc", { remap = true })
-    -- vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
+    vim.keymap.set(
+      "n",
+      "<leader><C-c>",
+      "gcgc",
+      { remap = true, desc = "Uncomment" }
+    )
+    vim.keymap.set("n", "<C-S-c>", "<Plug>(comment_toggle_blockwise_current)")
     vim.keymap.set("x", "<C-S-c>", "<Plug>(comment_toggle_blockwise_visual)")
-    vim.keymap.set("x", "aC", require("vim._comment").textobject)
+    vim.keymap.set("x", "agc", require("vim._comment").textobject)
   end,
 }
