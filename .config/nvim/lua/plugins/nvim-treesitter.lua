7a869b499f3bf89d0b06d4b0fc254085cd3ee327
jshu <jasonshu@terpmail.umd.edu>
2024-07-27 20:08:00 -0400

    tree

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -14,10 +14,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<leader><leader>",
-          node_incremental = "<leader><leader>",
+          init_selection = "m",
+          node_incremental = "m",
           scope_incremental = false,
-          node_decremental = "<bs>",
+          node_decremental = "M",
         },
       },
       -- textobjects = {
