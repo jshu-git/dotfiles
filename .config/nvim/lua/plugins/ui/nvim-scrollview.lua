6d6b4d22404ffa4fb50936a03910227ff8378203
jshu <jasonshu@terpmail.umd.edu>
2024-07-27 21:34:45 -0400

    test mini.map

--- .config/nvim/lua/plugins/ui/nvim-scrollview.lua
+++ .config/nvim/lua/plugins/ui/nvim-scrollview.lua
@@ -6,4 +6,31 @@ return {
       search_symbol = '—',
     })
   end,
+
+  -- {
+  --   'echasnovski/mini.map',
+  --   config = function()
+  --     local map = require('mini.map')
+  --     map.setup({
+  --       integrations = {
+  --         map.gen_integration.builtin_search(),
+  --         map.gen_integration.diff(),
+  --         map.gen_integration.diagnostic({
+  --           error = 'DiagnosticFloatingError',
+  --           warn = 'DiagnosticFloatingWarn',
+  --           info = 'DiagnosticFloatingInfo',
+  --           hint = 'DiagnosticFloatingHint',
+  --         }),
+  --       },
+  --       symbols = {
+  --         -- scroll_line = '',
+  --         -- scroll_view = '▐',
+  --       },
+  --       window = {
+  --         -- winblend = 100,
+  --         show_integration_count = false,
+  --       },
+  --     })
+  --   end,
+  -- },
 }
