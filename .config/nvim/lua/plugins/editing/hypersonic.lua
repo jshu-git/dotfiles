c590efdc696ee2b189529631b82643582ed02dbf
jshu <jasonshu@terpmail.umd.edu>
2024-07-27 20:08:10 -0400

    hypersonic

---
+++ .config/nvim/lua/plugins/editing/hypersonic.lua
@@ -0,0 +1,15 @@
+return {
+  "tomiis4/Hypersonic.nvim",
+  keys = {
+    { "gR", mode = { "n", "x" } },
+  },
+  cmd = "Hypersonic",
+  config = function()
+    require("hypersonic").setup({
+      border = "single",
+      enable_cmdline = false,
+    })
+    vim.keymap.set("n", "gR", "<cmd>Hypersonic<CR>", { desc = "Regex" })
+    vim.keymap.set("x", "gR", ":'<,'>Hypersonic<CR>", { desc = "Regex" })
+  end,
+}
