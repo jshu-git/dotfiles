524154fb768876667a9d30b4d1303c67d430a9e6
jshu <jasonshu@terpmail.umd.edu>
2024-07-29 11:54:24 -0400

    tree

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,8 +5,27 @@ return {
     { 'nvim-treesitter/nvim-treesitter-context', opts = { max_lines = 1 } },
   },
   config = function()
+    -- work
+    if vim.env.SSH_CLIENT ~= nil then
+      local parser_config =
+        require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.lua = {
+        install_info = {
+          url = '~/.packages/parsers/lua/',
+          files = { 'src/parser.c', 'src/scanner.c' },
+        },
+      }
+      ---@diagnostic disable-next-line: inject-field
+      parser_config.python = {
+        install_info = {
+          url = '~/.packages/parsers/python/',
+          files = { 'src/parser.c', 'src/scanner.c' },
+        },
+        filetype = 'py',
+      }
+    end
+
     require('nvim-treesitter.configs').setup({
-      auto_install = true,
       highlight = {
         enable = true,
       },
