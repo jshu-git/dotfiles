6f2ae36c03d84cca19488bcf38a8a5fa8a1185bb
jshu <jasonshu@terpmail.umd.edu>
2024-07-30 22:20:26 -0400

    md

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,33 +1,39 @@
 return {
-  {
-    'MeanderingProgrammer/markdown.nvim',
-    version = 'v5.0.0',
-    ft = 'markdown',
-    dependencies = {
-      'echasnovski/mini.icons',
-    },
-    config = function()
-      local markdown = require('render-markdown')
-      markdown.setup({
-        enabled = false,
-        -- not working in visual for some reason
-        -- render_modes = { 'n', 'c', 'x' },
-        anti_conceal = {
-          enabled = false,
-        },
-      })
-
-      vim.keymap.set(
-        'n',
-        '<leader>tM',
-        markdown.toggle,
-        { desc = 'Toggle Markdown' }
-      )
-    end,
-  },
-  {
-    'ravibrock/spellwarn.nvim',
-    config = true,
-    enabled = false,
-  },
+  -- {
+  --   'OXY2DEV/markview.nvim',
+  --   ft = 'markdown',
+  --   config = function()
+  --     require('markview').setup({})
+  --   end,
+  -- },
+  -- {
+  --   'MeanderingProgrammer/markdown.nvim',
+  --   ft = 'markdown',
+  --   dependencies = {
+  --     'echasnovski/mini.icons',
+  --   },
+  --   config = function()
+  --     local markdown = require('render-markdown')
+  --     markdown.setup({
+  --       enabled = false,
+  --       -- not working in visual for some reason
+  --       -- render_modes = { 'n', 'c', 'x' },
+  --       anti_conceal = {
+  --         enabled = false,
+  --       },
+  --     })
+  --
+  --     vim.keymap.set(
+  --       'n',
+  --       '<leader>tM',
+  --       markdown.toggle,
+  --       { desc = 'Toggle Markdown' }
+  --     )
+  --   end,
+  -- },
+  -- {
+  --   'ravibrock/spellwarn.nvim',
+  --   config = true,
+  --   enabled = false,
+  -- },
 }
