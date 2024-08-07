6aa4b6c8482e67f29b10848eb957291546abf9b7
jshu <jasonshu@terpmail.umd.edu>
2024-08-06 20:56:56 -0400

    return of buffer_manager

---
+++ .config/nvim/lua/plugins/buffer_manager.lua
@@ -0,0 +1,28 @@
+return {
+  'j-morano/buffer_manager.nvim',
+  config = function()
+    require('buffer_manager').setup({
+      select_menu_item_commands = {
+        v = {
+          key = '<C-v>',
+          command = 'vsplit',
+        },
+        s = {
+          key = '<C-h>',
+          command = 'split',
+        },
+      },
+      -- width = math.floor(require('config.utils').popup.width * 0.3),
+      -- height = math.floor(require('config.utils').popup.height * 0.3),
+      loop_nav = false,
+      highlight = 'Normal:FloatBorder',
+      borderchars = require('config.utils').border,
+      show_indicators = 'after',
+    })
+
+    local bmui = require('buffer_manager.ui')
+    vim.keymap.set('n', '<Tab>', bmui.toggle_quick_menu, { silent = true })
+    -- map('n', '<M-j>', bmui.nav_next, opts)
+    -- map('n', '<M-k>', bmui.nav_prev, opts)
+  end,
+}
--- .config/nvim/lua/plugins/persisted.lua
+++ .config/nvim/lua/plugins/persisted.lua
@@ -3,14 +3,14 @@ return {
   lazy = false,
   config = function()
     -- barbar
-    vim.opt.sessionoptions:append('globals')
-    vim.api.nvim_create_autocmd({ 'User' }, {
-      pattern = 'PersistedSavePre',
-      group = vim.api.nvim_create_augroup('PersistedHooks', {}),
-      callback = function()
-        vim.api.nvim_exec_autocmds('User', { pattern = 'SessionSavePre' })
-      end,
-    })
+    -- vim.opt.sessionoptions:append('globals')
+    -- vim.api.nvim_create_autocmd({ 'User' }, {
+    --   pattern = 'PersistedSavePre',
+    --   group = vim.api.nvim_create_augroup('PersistedHooks', {}),
+    --   callback = function()
+    --     vim.api.nvim_exec_autocmds('User', { pattern = 'SessionSavePre' })
+    --   end,
+    -- })
 
     local persisted = require('persisted')
     persisted.setup({
