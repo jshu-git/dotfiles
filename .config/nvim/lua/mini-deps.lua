4cd89ba2fe3c12c6ec8a51200a5938e6df105c61
jshu <jasonshu@terpmail.umd.edu>
2024-08-24 19:53:48 -0400

    deps

--- .config/nvim/lua/mini-deps.lua
+++ .config/nvim/lua/mini-deps.lua
@@ -25,15 +25,15 @@ vim.keymap.set('n', '<leader>lw', deps.snap_save, { desc = 'Deps: Snapshot (Set)
 vim.keymap.set('n', '<leader>lr', deps.snap_load, { desc = 'Deps: Snapshot (Load)' })
 
 now(function()
+  -- session manager
   add('olimorris/persisted.nvim')
   require('plugins.persisted')
 
+  -- ui
   add('gbprod/nord.nvim')
   require('plugins.colorscheme')
-
   add('nvim-lualine/lualine.nvim')
   require('plugins.lualine')
-
   local icons = require('mini.icons')
   icons.setup({
     filetype = {
@@ -43,53 +43,50 @@ now(function()
   })
   icons.mock_nvim_web_devicons()
 
+  -- editing
   add('nmac427/guess-indent.nvim')
   require('guess-indent').setup({})
-
   add('sitiom/nvim-numbertoggle')
-
-  -- HACK: this is necessary because lsp binds K and can't be disabled
+  -- HACK: this is necessary because lsp binds K and can't be disabled?
   require('plugins.editing.mini-move')
+
+  -- work
+  add('ojroques/nvim-osc52')
+  require('plugins.editing.nvim-osc52')
+
+  -- lsp
+  add({
+    source = 'neovim/nvim-lspconfig',
+    depends = { 'hrsh7th/cmp-nvim-lsp' },
+  })
+  require('plugins.nvim-lspconfig')
+  add('folke/lazydev.nvim')
+  require('lazydev').setup({})
 end)
 
 later(function()
+  -- treesitter
   add({
     source = 'nvim-treesitter/nvim-treesitter',
     depends = { 'nvim-treesitter/nvim-treesitter-context' },
   })
   require('plugins.nvim-treesitter')
 
-  require('plugins.mini-files')
-  require('plugins.mini-pick')
-
+  -- mini
   require('mini.extra').setup()
   local misc = require('mini.misc')
   misc.setup_restore_cursor()
   misc.setup_termbg_sync()
   vim.keymap.set('n', '<C-w>m', misc.zoom, { desc = 'Toggle Maximize' })
-end)
 
--- lsp
-now(function()
-  add({
-    source = 'neovim/nvim-lspconfig',
-    depends = { 'hrsh7th/cmp-nvim-lsp' },
-  })
-  require('plugins.nvim-lspconfig')
-
-  add('folke/lazydev.nvim')
-  require('lazydev').setup({})
-end)
-later(function()
+  -- lsp
   add('dnlhc/glance.nvim')
   add('smjonas/inc-rename.nvim')
   add('stevearc/aerial.nvim')
   add('stevearc/conform.nvim')
   vim.cmd('runtime! lua/plugins/lsp/*.lua')
-end)
 
--- cmp
-later(function()
+  -- cmp
   add({
     source = 'hrsh7th/nvim-cmp',
     depends = {
@@ -110,10 +107,8 @@ later(function()
   -- work
   add('zbirenbaum/copilot.lua')
   vim.cmd('runtime! lua/plugins/cmp/*.lua')
-end)
 
--- ui
-later(function()
+  -- ui
   add('0xAdk/full_visual_line.nvim')
   add('MeanderingProgrammer/render-markdown.nvim')
   add('OXY2DEV/helpview.nvim')
@@ -130,10 +125,8 @@ later(function()
     depends = { 'MunifTanjim/nui.nvim' },
   })
   vim.cmd('runtime! lua/plugins/ui/*.lua')
-end)
 
--- editing
-later(function()
+  -- editing
   add('2kabhishek/nerdy.nvim')
   add('BranimirE/fix-auto-scroll.nvim')
   add('MagicDuck/grug-far.nvim')
@@ -144,21 +137,15 @@ later(function()
   add('monaqa/dial.nvim')
   add('numToStr/Comment.nvim')
   add('tomiis4/Hypersonic.nvim')
-  -- work
-  add('ojroques/nvim-osc52')
   vim.cmd('runtime! lua/plugins/editing/*.lua')
-end)
 
--- operators
-later(function()
+  -- operators
   add('NStefan002/visual-surround.nvim')
   add('chrisgrieser/nvim-rip-substitute')
   add('gbprod/substitute.nvim')
   vim.cmd('runtime! lua/plugins/operators/*.lua')
-end)
 
--- git
-later(function()
+  -- git
   add('sindrets/diffview.nvim')
   add('jshu-git/blame.nvim')
   add('kdheepak/lazygit.nvim')
