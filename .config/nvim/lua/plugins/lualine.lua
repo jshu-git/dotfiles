40a319400879224ba2cf4a28b992e977f81fd740
jshu <jasonshu@terpmail.umd.edu>
2024-08-25 19:39:41 -0400

    lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,51 +1,45 @@
-local total_lines = function()
-  return vim.api.nvim_buf_line_count(0) .. 'L'
-end
 local file_size = function()
-  local size = vim.fn.getfsize(vim.fn.expand('%:p')) / 1024
-  if size < 0 then
+  local size = vim.fn.getfsize(vim.fn.getreg('%'))
+  if size <= 0 then
     return ''
+  elseif size < 1024 then
+    return string.format('%dB', size)
+  elseif size < 1048576 then
+    return string.format('%.2fKB', size / 1024)
+  else
+    return string.format('%.2fMB', size / 1048576)
   end
-  return string.format('%.2f', size) .. 'KB'
+end
+
+local total_lines = function()
+  local lines = vim.api.nvim_buf_line_count(0)
+  return string.format('%dL', lines)
 end
 
 require('lualine').setup({
   options = {
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = { 'alpha' },
+    component_separators = '',
+    -- section_separators = '',
   },
   sections = {
-    lualine_a = {
-      { 'mode' },
-    },
-    lualine_b = {
-      { 'branch' },
-      { 'diff' },
-      { 'diagnostics' },
-    },
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = {
-      { 'filename', path = 1 },
-    },
-    lualine_x = {
-      { file_size },
-    },
-    lualine_y = {
-      { 'filetype' },
-    },
-    lualine_z = {
       {
         function()
           if require('grapple').exists() then
-            return ' ' .. require('grapple').name_or_index()
+            return ' '
           else
             return ''
           end
         end,
         padding = { left = 1, right = 0 },
       },
-      { total_lines },
+      { 'filename', path = 1 },
     },
+    lualine_x = { file_size },
+    lualine_y = { 'filetype' },
+    lualine_z = { total_lines },
   },
   inactive_sections = {
     lualine_a = {},
