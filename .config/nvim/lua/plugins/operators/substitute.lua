0b36f4bb103dba97483742e24a71689f642fd4af
jshu <jasonshu@terpmail.umd.edu>
2024-08-24 21:53:33 -0400

    clean

--- .config/nvim/lua/plugins/operators/substitute.lua
+++ .config/nvim/lua/plugins/operators/substitute.lua
@@ -1,18 +1,11 @@
-local deps = require('mini.deps')
-local add, now, later = deps.add, deps.now, deps.later
-
-later(function()
-  add('gbprod/substitute.nvim')
-
-  local substitute = require('substitute')
-  substitute.setup({
-    preserve_cursor_position = true,
-    highlight_substituted_text = {
-      timer = require('utils').highlight_timeout,
-    },
-  })
-  vim.keymap.set('n', 's', substitute.operator)
-  vim.keymap.set('n', 'ss', substitute.line, { desc = 'Substitute (Line)' })
-  vim.keymap.set('n', 'S', substitute.eol)
-  vim.keymap.set('x', 'ss', substitute.visual, { desc = 'Substitute (Visual)' })
-end)
+local substitute = require('substitute')
+substitute.setup({
+  preserve_cursor_position = true,
+  highlight_substituted_text = {
+    timer = require('utils').highlight_timeout,
+  },
+})
+vim.keymap.set('n', 's', substitute.operator)
+vim.keymap.set('n', 'ss', substitute.line, { desc = 'Substitute (Line)' })
+vim.keymap.set('n', 'S', substitute.eol)
+vim.keymap.set('x', 'ss', substitute.visual, { desc = 'Substitute (Visual)' })
