3a82376973720065e63dfd4b1e599a1cc29bb1cc
jshu <jasonshu@terpmail.umd.edu>
2024-08-26 17:27:21 -0400

    perf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,20 @@ if vim.env.SSH_CLIENT ~= nil then
   vim.g.editorconfig = false
 end
 
+vim.loader.enable()
+vim.g.loaded_fzf = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_tutor_mode_plugin = 1
+
 local safe_require = require('utils').safe_require
 safe_require('config.options')
 safe_require('config.autocommands')
