a785c281e42aee360e4fb5a4fbc02a58996961bf
jshu <jasonshu@terpmail.umd.edu>
2024-08-26 15:25:42 -0400

    copilot

--- .config/nvim/lua/plugins/cmp/neocodeium.lua
+++ .config/nvim/lua/plugins/cmp/neocodeium.lua
@@ -4,11 +4,9 @@ end
 
 local neocodeium = require('neocodeium')
 neocodeium.setup({
-  -- manual = true,
   silent = true,
   filetypes = {
     ['*'] = true,
-    ['rip-substitute'] = false,
     ['grug-far'] = false,
   },
   debounce = true,
@@ -23,7 +21,7 @@ vim.keymap.set('i', '<Tab>', function()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
   end
 end)
-vim.keymap.set('i', '<S-Tab>', neocodeium.clear)
+vim.keymap.set('i', '<C-\\>', neocodeium.clear)
 vim.keymap.set('i', '<A-l>', neocodeium.accept_word)
 vim.keymap.set('i', '<A-n>', function()
   neocodeium.cycle_or_complete(1)
