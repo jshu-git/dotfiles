1acc89104017f6daeed890053f1754b478c95b06
jshu <jasonshu@terpmail.umd.edu>
2024-08-26 22:57:36 -0400

    mini.statusline!

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,52 +1,86 @@
 require('nord').setup({
   diff = { mode = 'fg' },
   search = { theme = 'vscode' },
-  on_highlights = function(highlights, colors)
-    highlights.LspInfoBorder = { link = 'FloatBorder' }
-    highlights.QuickFixLine = { link = 'CursorLine' }
+  on_highlights = function(hl, c)
+    -- defaults = {
+    --   polar_night = {
+    --     origin = "#2E3440", -- nord0
+    --     bright = "#3B4252", -- nord1
+    --     brighter = "#434C5E", -- nord2
+    --     brightest = "#4C566A", -- nord3
+    --     light = "#616E88", -- out of palette
+    --   },
+    --   snow_storm = {
+    --     origin = "#D8DEE9", -- nord4
+    --     brighter = "#E5E9F0", -- nord5
+    --     brightest = "#ECEFF4", -- nord6
+    --   },
+    --   frost = {
+    --     polar_water = "#8FBCBB", -- nord7
+    --     ice = "#88C0D0", -- nord8
+    --     artic_water = "#81A1C1", -- nord9
+    --     artic_ocean = "#5E81AC", -- nord10
+    --   },
+    --   aurora = {
+    --     red = "#BF616A", -- nord11
+    --     orange = "#D08770", -- nord12
+    --     yellow = "#EBCB8B", -- nord13
+    --     green = "#A3BE8C", -- nord14
+    --     purple = "#B48EAD", -- nord15
+    --   },
+    -- }
+    hl.LspInfoBorder = { link = 'FloatBorder' }
+    hl.QuickFixLine = { link = 'CursorLine' }
+    hl.CmpItemAbbrMatch = { link = 'DiffText' }
 
     -- mini
-    highlights.MiniIconsAzure = { fg = colors.frost.artic_water }
-    highlights.MiniIconsBlue = { fg = colors.frost.artic_ocean }
-    highlights.MiniIconsCyan = { fg = colors.frost.ice }
-    highlights.MiniIconsGreen = { fg = colors.aurora.green }
-    highlights.MiniIconsGrey = { fg = colors.polar_night.light }
-    highlights.MiniIconsOrange = { fg = colors.aurora.orange }
-    highlights.MiniIconsPurple = { fg = colors.aurora.purple }
-    highlights.MiniIconsRed = { fg = colors.aurora.red }
-    highlights.MiniIconsYellow = { fg = colors.aurora.yellow }
-    highlights.MiniDiffSignAdd = { link = 'GitSignsAddNr' }
-    highlights.MiniDiffSignChange = { link = 'GitSignsChangeNr' }
-    highlights.MiniDiffSignDelete = { link = 'GitSignsDeleteNr' }
-    highlights.MiniPickPrompt = { link = 'Normal' }
-    highlights.MiniPickMatchRanges = { link = 'DiffText' }
-    highlights.CmpItemAbbrMatch = { link = 'DiffText' }
+    hl.MiniIconsAzure = { fg = c.frost.artic_water }
+    hl.MiniIconsBlue = { fg = c.frost.artic_ocean }
+    hl.MiniIconsCyan = { fg = c.frost.ice }
+    hl.MiniIconsGreen = { fg = c.aurora.green }
+    hl.MiniIconsGrey = { fg = c.polar_night.light }
+    hl.MiniIconsOrange = { fg = c.aurora.orange }
+    hl.MiniIconsPurple = { fg = c.aurora.purple }
+    hl.MiniIconsRed = { fg = c.aurora.red }
+    hl.MiniIconsYellow = { fg = c.aurora.yellow }
+
+    hl.MiniDiffSignAdd = { link = 'GitSignsAddNr' }
+    hl.MiniDiffSignChange = { link = 'GitSignsChangeNr' }
+    hl.MiniDiffSignDelete = { link = 'GitSignsDeleteNr' }
+
+    hl.MiniPickPrompt = { link = 'Normal' }
+    hl.MiniPickMatchRanges = { link = 'DiffText' }
+
+    hl.MiniStatuslineModeNormal = { fg = c.polar_night.bright, bg = c.frost.ice }
+    hl.MiniStatuslineModeVisual = { fg = c.polar_night.bright, bg = c.frost.polar_water }
+    hl.MiniStatuslineModeInsert = { fg = c.polar_night.bright, bg = c.snow_storm.origin }
+    hl.MiniStatuslineModeCommand = { fg = c.polar_night.bright, bg = c.aurora.purple }
 
     -- copilot
-    highlights.NeoCodeiumSuggestion = { link = 'NonText' }
-    highlights.NeoCodeiumLabel = { link = 'Search' }
-    highlights.CopilotSuggestion = { link = 'NonText' }
-    highlights.CopilotAnnotation = { link = 'Search' }
+    hl.NeoCodeiumSuggestion = { link = 'NonText' }
+    hl.NeoCodeiumLabel = { link = 'Search' }
+    hl.CopilotSuggestion = { link = 'NonText' }
+    hl.CopilotAnnotation = { link = 'Search' }
 
     -- sentiment
-    highlights.MatchParen = { link = 'Underlined' }
+    hl.MatchParen = { link = 'Underlined' }
 
     -- noice
-    highlights.NoiceCmdLinePopupBorder = { link = 'FloatBorder' }
+    hl.NoiceCmdLinePopupBorder = { link = 'FloatBorder' }
 
     -- treesitter
-    highlights.TreesitterContext = { link = 'CursorLine' }
+    hl.TreesitterContext = { link = 'CursorLine' }
 
     -- glance
-    highlights.GlancePreviewEndOfBuffer = { link = 'EndOfBuffer' }
+    hl.GlancePreviewEndOfBuffer = { link = 'EndOfBuffer' }
 
     -- paren-hint
     -- highlights.ParenHint = { fg = highlights.Comment.fg, bg = highlights.CursorLine.bg }
-    highlights.ParenHint = { link = 'NonText' }
+    hl.ParenHint = { link = 'NonText' }
 
     -- grapple
-    highlights.GrappleFooter = { link = 'FloatBorder' }
-    highlights.GrappleCurrent = { link = 'Normal' }
+    hl.GrappleFooter = { link = 'FloatBorder' }
+    hl.GrappleCurrent = { link = 'Normal' }
   end,
 })
 
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,52 +1,102 @@
-local file_size = function()
-  local size = vim.fn.getfsize(vim.fn.getreg('%'))
-  if size <= 0 then
-    return ''
-  elseif size < 1024 then
-    return string.format('%dB', size)
-  elseif size < 1048576 then
-    return string.format('%.2fKB', size / 1024)
-  else
-    return string.format('%.2fMB', size / 1048576)
-  end
-end
+local statusline = require('mini.statusline')
+statusline.setup({
+  content = {
+    active = function()
+      -- a
+      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
 
-local total_lines = function()
-  local lines = vim.api.nvim_buf_line_count(0)
-  return string.format('%dL', lines)
-end
+      -- b
+      -- local git = statusline.section_git({ trunc_width = 40 })
+      -- local diff = statusline.section_diff({ trunc_width = 75 })
+      -- local custom_diff = function()
+      --   return vim.b.minidiff_summary_string or ''
+      -- end
+      local custom_git = function()
+        return vim.b.minigit_summary_string and (' ' .. vim.b.minigit_summary_string) or ''
+      end
+      local diagnostics = statusline.section_diagnostics({
+        trunc_width = 75,
+        icon = '',
+        signs = {
+          ERROR = require('utils').signs.Error,
+          WARN = require('utils').signs.Warn,
+          INFO = require('utils').signs.Info,
+          HINT = require('utils').signs.Hint,
+        },
+      })
+      -- c
+      local filename = statusline.section_filename({ trunc_width = 140 })
 
-require('lualine').setup({
-  options = {
-    component_separators = '',
-    -- section_separators = '',
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = {
-      {
-        function()
-          if require('grapple').exists() then
-            return ' '
-          else
-            return ''
-          end
-        end,
-        padding = { left = 1, right = 0 },
-      },
-      { 'filename', path = 1 },
-    },
-    lualine_x = { file_size },
-    lualine_y = { 'filetype' },
-    lualine_z = { total_lines },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
+      -- x
+      local file_size = function()
+        local size = vim.fn.getfsize(vim.fn.getreg('%'))
+        if size <= 0 then
+          return ''
+        elseif size < 1024 then
+          return string.format('%dB', size)
+        elseif size < 1048576 then
+          return string.format('%.2fKB', size / 1024)
+        else
+          return string.format('%.2fMB', size / 1048576)
+        end
+      end
+
+      -- y
+      local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+      local custom_fileinfo = string.match(fileinfo, '^[^%s]+%s+[^%s]+')
+
+      -- z
+      local total_lines = function()
+        local lines = vim.api.nvim_buf_line_count(0)
+        return string.format('%dL', lines)
+      end
+
+      return statusline.combine_groups({
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { custom_git(), diagnostics } },
+        '%<',
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=',
+        { hl = 'MiniStatuslineFilename', strings = { file_size() } },
+        { hl = 'MiniStatuslineFileinfo', strings = { custom_fileinfo } },
+        { hl = mode_hl, strings = { total_lines() } },
+      })
+    end,
+    inactive = nil,
   },
 })
+
+-- require('lualine').setup({
+--   options = {
+--     component_separators = '',
+--     -- section_separators = '',
+--   },
+--   sections = {
+--     lualine_a = { 'mode' },
+--     lualine_b = { 'branch', 'diff', 'diagnostics' },
+--     lualine_c = {
+--       {
+--         function()
+--           if require('grapple').exists() then
+--             return ' '
+--           else
+--             return ''
+--           end
+--         end,
+--         padding = { left = 1, right = 0 },
+--       },
+--       { 'filename', path = 1 },
+--     },
+--     lualine_x = { file_size },
+--     lualine_y = { 'filetype' },
+--     lualine_z = { total_lines },
+--   },
+--   inactive_sections = {
+--     lualine_a = {},
+--     lualine_b = {},
+--     lualine_c = {},
+--     lualine_x = {},
+--     lualine_y = {},
+--     lualine_z = {},
+--   },
+-- })
