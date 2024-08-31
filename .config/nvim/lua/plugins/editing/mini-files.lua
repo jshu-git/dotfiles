8364ad0eb642a762bea02180ebf5ffbd9114d492
jshu <jasonshu@terpmail.umd.edu>
2024-08-31 14:44:25 -0400

    mini files

--- .config/nvim/lua/plugins/editing/mini-files.lua
+++ .config/nvim/lua/plugins/editing/mini-files.lua
@@ -49,16 +49,19 @@ end
 
 -- open in split
 local map_split = function(buf_id, lhs, direction)
-  vim.keymap.set('n', lhs, function()
-    local new_target_window
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.api.nvim_win_call(files.get_target_window(), function()
+  local rhs = function()
+    local cur_target = files.get_explorer_state().target_window
+    local new_target = vim.api.nvim_win_call(cur_target, function()
       vim.cmd(direction .. ' split')
-      new_target_window = vim.api.nvim_get_current_win()
+      return vim.api.nvim_get_current_win()
     end)
-    files.set_target_window(new_target_window)
+    files.set_target_window(new_target)
+
+    -- close explorer
     files.go_in({ close_on_file = true })
-  end, { buffer = buf_id, desc = 'Split ' .. direction })
+  end
+  -- Adding `desc` will result into `show_help` entries
+  vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = 'Split ' .. direction })
 end
 
 vim.api.nvim_create_autocmd('User', {
