ba636242fc8b42a9398a04da95b31d9d5bec2fc7
jshu <jasonshu@terpmail.umd.edu>
2024-08-31 20:34:36 -0400

    vim.g

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,11 +11,12 @@ vim.g.loaded_tarPlugin = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_2html_plugin = 1
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
 vim.g.loaded_remote_plugins = 1
-vim.g.loaded_python3_provider = 0
 vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
 local safe_require = require('utils').safe_require
 safe_require('config.options')
