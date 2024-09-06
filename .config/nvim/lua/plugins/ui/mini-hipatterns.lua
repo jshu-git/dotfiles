32626fe5d30e1458cc15e08ae48edcb68ed211a4
jshu <jasonshu@terpmail.umd.edu>
2024-09-05 22:12:59 -0400

    hipatterns

--- .config/nvim/lua/plugins/ui/mini-hipatterns.lua
+++ .config/nvim/lua/plugins/ui/mini-hipatterns.lua
@@ -18,11 +18,6 @@ local hipatterns = require('mini.hipatterns')
 --   end
 -- end
 
--- local highlighters = vim.tbl_deep_extend('force', {
---   hex_color = hipatterns.gen_highlighter.hex_color(),
--- }, {})
--- }, nord_colors)
-
 hipatterns.setup({
   highlighters = {
     hex_color = hipatterns.gen_highlighter.hex_color(),
@@ -38,34 +33,3 @@ hipatterns.setup({
     },
   },
 })
-
--- local nord_colors = {}
--- -- flatten nord.colors palette
--- for palette, colors in pairs(require('nord.colors').palette) do
---   if palette ~= 'none' then
---     ---@diagnostic disable-next-line: param-type-mismatch
---     for color, hex in pairs(colors) do
---       nord_colors[palette .. '.' .. color] = hex
---     end
---   end
--- end
--- print(vim.inspect(nord_colors))
--- {
---   ["aurora.green"] = "#A3BE8C",
---   ["aurora.orange"] = "#D08770",
---   ["aurora.purple"] = "#B48EAD",
---   ["aurora.red"] = "#BF616A",
---   ["aurora.yellow"] = "#EBCB8B",
---   ["frost.artic_ocean"] = "#5E81AC",
---   ["frost.artic_water"] = "#81A1C1",
---   ["frost.ice"] = "#88C0D0",
---   ["frost.polar_water"] = "#8FBCBB",
---   ["polar_night.bright"] = "#3B4252",
---   ["polar_night.brighter"] = "#434C5E",
---   ["polar_night.brightest"] = "#4C566A",
---   ["polar_night.light"] = "#616E88",
---   ["polar_night.origin"] = "#2E3440",
---   ["snow_storm.brighter"] = "#E5E9F0",
---   ["snow_storm.brightest"] = "#ECEFF4",
---   ["snow_storm.origin"] = "#D8DEE9"
--- }
