50faf69b218c872b466edf609e0fd3f46e3d01ff
jshu <jasonshu@terpmail.umd.edu>
2024-09-07 18:16:45 -0400

    loader

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,20 +3,23 @@ if vim.env.SSH_CLIENT ~= nil then
 end
 
 vim.loader.enable()
-vim.g.loaded_fzf = 1
+-- plugins
+vim.g.loaded_2html_plugin = 1
 vim.g.loaded_gzip = 1
+vim.g.loaded_man = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zipPlugin = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_2html_plugin = 1
 vim.g.loaded_remote_plugins = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_node_provider = 0
+vim.g.loaded_zipPlugin = 1
+-- providers
+vim.g.loaded_node_provider = 1
+vim.g.loaded_perl_provider = 1
+vim.g.loaded_python3_provider = 1
+vim.g.loaded_ruby_provider = 1
 
 local safe_require = require('utils').safe_require
 safe_require('config.options')
