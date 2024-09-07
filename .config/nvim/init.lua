80d98ebec22936ae2860adf853ce83faca523250
jshu <jasonshu@terpmail.umd.edu>
2024-09-07 18:58:03 -0400

    loader

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,10 +11,14 @@ vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
 vim.g.loaded_spellfile_plugin = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_zipPlugin = 1
+
+vim.g.did_load_ftplugin = 1
+
 -- providers
 vim.g.loaded_node_provider = 1
 vim.g.loaded_perl_provider = 1
