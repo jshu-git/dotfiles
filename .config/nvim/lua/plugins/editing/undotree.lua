93a80f5ebd4d25e4bf74bce074d1beb15aac4d4d
jshu <jasonshu@terpmail.umd.edu>
2024-09-09 17:55:56 -0400

    undotree

--- .config/nvim/lua/plugins/editing/undotree.lua
+++ .config/nvim/lua/plugins/editing/undotree.lua
@@ -1,20 +1,15 @@
 vim.g.undotree_WindowLayout = 3
-vim.g.undotree_ShortIndicators = 1
 vim.g.undotree_DiffAutoOpen = 0
 vim.g.undotree_SetFocusWhenToggle = 1
 vim.g.undotree_TreeNodeShape = '󰧞'
-vim.g.undotree_TreeReturnShape = '╲'
 vim.g.undotree_TreeVertShape = '│'
+vim.g.undotree_TreeReturnShape = '╲'
 vim.g.undotree_TreeSplitShape = '╱'
+vim.g.undotree_ShortIndicators = 1
 vim.g.undotree_HelpLine = 0
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undo Tree' })
 
--- close undotree with <esc>
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-    'undotree',
-  },
-  callback = function(event)
-    vim.keymap.set('n', '<esc>', vim.cmd.UndotreeToggle, { buffer = event.buf, silent = true })
-  end,
-})
+vim.g.Undotree_CustomMap = function()
+  vim.keymap.set('n', '<esc>', '<Plug>UndotreeClose', { buffer = true, silent = true })
+  vim.keymap.set('n', 'U', '<Plug>UndotreeRedo', { buffer = true, silent = true })
+end
