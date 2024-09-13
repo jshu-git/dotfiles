ccae77c3a5ab24bf41f57e9d006fdd7114940d79
jshu <jasonshu@terpmail.umd.edu>
2024-09-13 10:53:19 -0400

    minijump2d

--- .config/nvim/lua/plugins/editing/mini-jump2d.lua
+++ .config/nvim/lua/plugins/editing/mini-jump2d.lua
@@ -5,17 +5,33 @@ jump2d.setup({
   },
 })
 -- jump to any word
-vim.keymap.set({ 'n', 'x', 'o' }, 'gw', function()
+-- vim.keymap.set({ 'n', 'x', 'o' }, 'gw', function()
+--   local opts = vim.tbl_deep_extend('force', jump2d.builtin_opts.word_start, {
+--     view = { n_steps_ahead = 1 },
+--     allowed_lines = { blank = false },
+--   })
+--   jump2d.start(opts)
+-- end, { desc = 'Jump (Word)' })
+
+-- jump to punctuation
+-- vim.keymap.set({ 'n', 'x', 'o' }, 'g.', function()
+--   jump2d.start({
+--     spotter = jump2d.gen_pattern_spotter('%p+'),
+--     allowed_lines = { blank = false },
+--   })
+-- end, { desc = 'Jump (Punctuation)' })
+
+vim.keymap.set({ 'n', 'x', 'o' }, 't', function()
   local opts = vim.tbl_deep_extend('force', jump2d.builtin_opts.word_start, {
     view = { n_steps_ahead = 1 },
     allowed_lines = { blank = false },
   })
   jump2d.start(opts)
-end, { desc = 'Jump (Word)' })
--- jump to punctuation
-vim.keymap.set({ 'n', 'x', 'o' }, 'g.', function()
+end)
+
+vim.keymap.set({ 'n', 'x', 'o' }, 'T', function()
   jump2d.start({
     spotter = jump2d.gen_pattern_spotter('%p+'),
     allowed_lines = { blank = false },
   })
-end, { desc = 'Jump (Punctuation)' })
+end)
