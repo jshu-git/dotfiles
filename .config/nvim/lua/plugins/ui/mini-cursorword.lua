05894cad5440be39aa2567b282dc838a98cd9828
jshu <jasonshu@terpmail.umd.edu>
2024-09-15 17:27:25 -0400

    cursorword

---
+++ .config/nvim/lua/plugins/ui/mini-cursorword.lua
@@ -0,0 +1,11 @@
+require('mini.cursorword').setup({ delay = 0 })
+vim.g.minicursorword_disable = true
+
+vim.keymap.set('n', 'M', function()
+  vim.g.minicursorword_disable = not vim.g.minicursorword_disable
+  -- hack
+  vim.api.nvim_command('normal! l')
+  vim.defer_fn(function()
+    vim.api.nvim_command('normal! h')
+  end, 1)
+end)
