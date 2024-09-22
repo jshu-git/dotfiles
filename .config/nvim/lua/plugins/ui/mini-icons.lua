23cac683e95f526586d9ca84a18234d4ad8db34c
jshu <jasonshu@terpmail.umd.edu>
2024-09-22 14:42:56 -0400

    icoons

--- .config/nvim/lua/plugins/ui/mini-icons.lua
+++ .config/nvim/lua/plugins/ui/mini-icons.lua
@@ -6,10 +6,10 @@ icons.setup({
     ['minideps-confirm'] = { glyph = '󰄵' },
     ['mininotify-history'] = { glyph = '󰋚' },
     -- extra
-    ['grug-far'] = { glyph = '󰾹' },
+    -- ['grug-far'] = { glyph = '󰾹' },
     undotree = { glyph = '󰕌' },
     grapple = { glyph = '' },
-    lspinfo = { glyph = '󰒓' },
+    lspinfo = { glyph = '󰋽' },
   },
 })
 icons.mock_nvim_web_devicons()
