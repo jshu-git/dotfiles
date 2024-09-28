fe074faccee3d06a49f669eed91812acaad6f366
jshu <jasonshu@terpmail.umd.edu>
2024-09-28 10:30:57 -0400

    nvim dark

---
+++ .config/nvim/lua/plugins/nvim-dark.lua
@@ -0,0 +1,79 @@
+-- https://old.reddit.com/r/neovim/comments/1bcsa1b/new_default_colorscheme_palette/
+-- https://github.com/neovim/neovim/blob/master/src/nvim/highlight_group.c#L2918
+
+-- #004c73 (Dark blue)
+-- #007373 (Dark cyan)
+-- #005523 (Dark green)
+-- #07080d (Dark grey1)
+-- #14161b (Dark grey2) -- Normal bg
+-- #2c2e33 (Dark grey3) -- CursorLine, Pmenu
+-- #4f5258 (Dark grey4) -- Visual, NonText, FloatBorder
+-- #470045 (Dark magenta)
+-- #590008 (Dark red)
+-- #6b5300 (Dark yellow)
+
+-- #a6dbff (Light blue)
+-- #8cf8f7 (Light cyan)
+-- #b3f6c0 (Light green)
+-- #eef1f8 (Light grey1)
+-- #e0e2ea (Light grey2)
+-- #c4c6cd (Light grey3)
+-- #9b9ea4 (Light grey4) -- Comment
+-- #ffcaff (Light magenta)
+-- #ffc0b9 (Light red)
+-- #fce094 (Light yellow)
+
+local highlights = {
+  CursorLineNr = { fg = 'white' },
+  LineNr = { fg = 'NvimDarkGrey3' },
+  QuickFixLine = { link = 'CursorLine' },
+  Comment = { fg = 'NvimLightGrey4', italic = true },
+
+  -- floating windows
+  NormalFloat = { link = 'Normal' },
+  FloatBorder = { link = 'NonText' },
+  FloatFooter = { link = 'FloatBorder' },
+  WinSeparator = { link = 'FloatBorder' },
+  LspInfoBorder = { link = 'FloatBorder' },
+
+  -- cmp
+  PmenuMatchSel = { fg = 'NvimLightCyan', bg = 'NvimDarkGrey4' },
+  PmenuSel = { link = 'Visual' },
+  PmenuMatch = { fg = 'NvimLightCyan', bg = 'NvimDarkGrey3' },
+
+  -- mini
+  -- MiniIconsAzure = { fg = 'NvimLightBlue' },
+  -- MiniIconsCyan = { fg = 'NvimLightCyan' },
+  -- MiniIconsGrey = { fg = 'NvimDarkGrey4' },
+  -- MiniIconsBlue = { fg = 'NvimLightBlue' },
+  -- MiniIconsRed = { fg = 'NvimDarkRed' },
+  -- MiniIconsOrange = { fg = 'NvimLightRed' },
+  -- MiniIconsYellow = { fg = 'NvimLightYellow' },
+  -- MiniIconsGreen = { fg = 'NvimLightGreen' },
+  -- MiniIconsPurple = { fg = 'NvimLightMagenta' },
+  -- MiniDiffSignAdd = { fg = 'NvimLightGreen' },
+  -- MiniDiffSignChange = { fg = 'NvimLightYellow' },
+  -- MiniDiffSignDelete = { fg = 'NvimDarkRed' },
+  MiniIndentScopeSymbol = { link = 'NonText' },
+
+  -- treesitter
+  -- TreesitterContext = { link = 'CursorLine' },
+
+  -- grapple
+  -- GrappleFooter = { link = 'NonText' },
+  -- GrappleCurrent = { link = 'Normal' },
+
+  -- treesitter
+  TreesitterContext = { link = 'CursorLine' },
+
+  -- telescope
+  TelescopeTitle = { link = 'FloatTitle' },
+  TelescopeBorder = { link = 'FloatBorder' },
+  TelescopeMatching = { link = 'Search' },
+  TelescopeMultiSelection = { link = 'Search' },
+  TelescopeSelection = { link = 'CursorLine' },
+}
+
+for group, hl in pairs(highlights) do
+  vim.api.nvim_set_hl(0, group, hl)
+end
