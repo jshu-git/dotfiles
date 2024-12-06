0690994cd0c4503ad1898d3bfff4424119fda5a3
jshu <jasonshu@terpmail.umd.edu>
2024-12-06 10:44:25 -0500

    smear

--- .config/nvim/lua/plugins/ui/smear-cursor.lua
+++ .config/nvim/lua/plugins/ui/smear-cursor.lua
@@ -1,7 +1,6 @@
 require('smear_cursor').setup({
   stiffness = 0.8, -- 0.6      [0, 1]
-  trailing_stiffness = 0.6, -- 0.3      [0, 1]
-  trailing_exponent = 0, -- 0.1      >= 0
+  trailing_stiffness = 0.5, -- 0.3      [0, 1]
   distance_stop_animating = 0.5, -- 0.1      > 0
   hide_target_hack = false, -- true     boolean
 })
