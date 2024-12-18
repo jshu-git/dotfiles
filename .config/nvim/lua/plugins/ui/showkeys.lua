e984fb0c730dab7b1be69e62a2cfa68f40756b3f
jshu <jasonshu@terpmail.umd.edu>
2024-12-18 18:56:36 -0500

    showkeys

--- .config/nvim/lua/plugins/ui/showkeys.lua
+++
@@ -1,7 +0,0 @@
-local showkeys = require('showkeys')
-showkeys.setup({
-  winopts = { row = 0 },
-  maxkeys = 5,
-  position = 'top-right',
-})
-vim.keymap.set('n', '<leader>tk', showkeys.toggle, { desc = 'Toggle Showkeys' })
