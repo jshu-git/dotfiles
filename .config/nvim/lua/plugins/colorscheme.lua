f19c0a8786bc00f3e824ecbaca8f09fa28d07696
jshu <jasonshu@terpmail.umd.edu>
2024-12-19 21:11:37 -0500

    colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,42 @@
+-- local black = '#011627'
+-- local white = '#c3ccdc'
+-- local black_blue = '#081e2f'
+-- local dark_blue = '#092236'
+-- local deep_blue = '#0e293f'
+-- local slate_blue = '#2c3043'
+-- local pickle_blue = '#38507a'
+-- local cello_blue = '#1f4462'
+-- local regal_blue = '#1d3b53'
+-- local steel_blue = '#4b6479'
+-- local grey_blue = '#7c8f8f'
+-- local cadet_blue = '#a1aab8'
+-- local ash_blue = '#acb4c2'
+-- local white_blue = '#d6deeb'
+-- local yellow = '#e3d18a'
+-- local peach = '#ffcb8b'
+-- local tan = '#ecc48d'
+-- local orange = '#f78c6c'
+-- local orchid = '#e39aa6'
+-- local red = '#fc514e'
+-- local watermelon = '#ff5874'
+-- local purple = '#ae81ff'
+-- local violet = '#c792ea'
+-- local lavender = '#b0b2f4'
+-- local blue = '#82aaff'
+-- local malibu = '#87bcff'
+-- local turquoise = '#7fdbca'
+-- local emerald = '#21c7a8'
+-- local green = '#a1cd5e'
+-- local cyan_blue = '#296596'
+-- local bay_blue = '#24567F'
+-- local kashmir_blue = '#4d618e'
+-- local plant_green = '#2a4e57'
+-- local palette = require('nightfly').palette
+
+vim.g.nightflyNormalFloat = true
+vim.g.nightflyUnderlineMatchParen = true
+vim.g.nightflyVirtualTextColor = true
+vim.g.nightflyWinSeparator = 2
 vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
     local highlights = {
@@ -11,6 +50,10 @@ vim.api.nvim_create_autocmd('ColorScheme', {
       MiniIndentScopeSymbol = { link = 'Whitespace' },
       MiniFilesTitle = { link = 'FloatBorder' },
       MiniFilesTitleFocused = { link = 'FloatTitle' },
+
+      -- telescope
+      TelescopeTitle = { link = 'FloatTitle' },
+      TelescopeSelection = { link = 'Visual' },
     }
 
     for group, hl in pairs(highlights) do
@@ -18,10 +61,6 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     end
   end,
 })
-vim.g.nightflyNormalFloat = true
-vim.g.nightflyUnderlineMatchParen = true
-vim.g.nightflyVirtualTextColor = true
-vim.g.nightflyWinSeparator = 2
 
 -- nvim-dark
 --[[
