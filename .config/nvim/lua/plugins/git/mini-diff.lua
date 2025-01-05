64c5ae78552def0c360bd6423f96b8d2a500f23f
jshu <jasonshu@terpmail.umd.edu>
2025-01-05 11:01:42 -0500

    minidiff

--- .config/nvim/lua/plugins/git/mini-diff.lua
+++ .config/nvim/lua/plugins/git/mini-diff.lua
@@ -13,18 +13,18 @@ diff.setup({
 vim.keymap.set('n', '<leader>gd', diff.toggle_overlay, { desc = 'Toggle Diff' })
 
 -- stage hunk
-vim.keymap.set('n', '<leader>ga', 'ghgh', { desc = 'Stage Hunk', remap = true })
-vim.keymap.set('x', '<leader>ga', 'gh', { desc = 'Stage Hunk (Visual)', remap = true })
+-- vim.keymap.set('n', '<leader>ga', 'ghgh', { desc = 'Stage Hunk', remap = true })
+-- vim.keymap.set('x', '<leader>ga', 'gh', { desc = 'Stage Hunk (Visual)', remap = true })
 
 -- reset hunks
 vim.keymap.set('n', '<leader>gr', 'gHgh', { desc = 'Reset Hunk', remap = true })
 vim.keymap.set('x', '<leader>gr', 'gH', { desc = 'Reset Hunk (Visual)', remap = true })
 vim.keymap.set('n', '<leader>gR', function()
   diff.do_hunks(0, 'reset')
-end, { desc = 'Reset (Buffer)' })
+end, { desc = 'Reset Hunk (Buffer)' })
 
 -- hunks to quickfix
-vim.keymap.set('n', '<leader>gq', function()
-  vim.fn.setqflist(diff.export('qf'))
-  vim.cmd('copen')
-end, { desc = 'Hunks (Quickfix)' })
+-- vim.keymap.set('n', '<leader>gq', function()
+--   vim.fn.setqflist(diff.export('qf'))
+--   vim.cmd('copen')
+-- end, { desc = 'Hunks (Quickfix)' })
