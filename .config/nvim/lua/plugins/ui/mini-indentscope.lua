090fcea53a8143f6d0e7e1f938c458755c939e47
jshu <jasonshu@terpmail.umd.edu>
2025-01-05 17:31:42 -0500

    indentscope

--- .config/nvim/lua/plugins/ui/mini-indentscope.lua
+++ .config/nvim/lua/plugins/ui/mini-indentscope.lua
@@ -3,10 +3,11 @@ require('mini.indentscope').setup({
     animation = function()
       return 0
     end,
+    priority = 0,
   },
   options = {
     indent_at_cursor = false,
     try_as_border = true,
   },
-  symbol = '▏',
+  symbol = '│',
 })
