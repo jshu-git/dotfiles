fa70c4803921aa174cc5acdb616ee095ff960c18
jshu <jasonshu@terpmail.umd.edu>
2025-01-08 20:15:38 -0500

    snacks

--- .config/nvim/lua/plugins/git/snacks.lua
+++ .config/nvim/lua/plugins/git/snacks.lua
@@ -1,9 +1,14 @@
-require('snacks').setup({
+local snacks = require('snacks')
+
+snacks.setup({
   gitbrowse = {
     open = function(url)
       vim.fn.setreg('+', url)
     end,
     remote_patterns = {
+      -- https://github.com/jshu-git/dotfiles.git/blob/main/.config/nvim/lua/plugins/git/snacks.lua#L21-L21
+      { '^https://github.com/(.*).git', 'https://github.com/%1' },
+
       -- (https://bitbucket.ngage.netapp.com)/scm/(op)/(api).git -> (https://bitbucket.ngage.netapp.com)/projects/(op)/repos/(api)/browse
       { '^(https://bitbucket%.ngage%.netapp%.com)/scm/(.*)/(.*).git$', '%1/projects/%2/repos/%3/browse' },
     },
@@ -14,3 +19,10 @@ require('snacks').setup({
     },
   },
 })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>gy', function()
+  snacks.gitbrowse()
+end, { desc = 'Copy URL' })
+vim.keymap.set({ 'n', 'x' }, '<leader>gb', function()
+  snacks.git.blame_line()
+end, { desc = 'Blame' })
