d516694c4f241ee826fb3334ba6f2cc39b02e460
jshu <jasonshu@terpmail.umd.edu>
2025-01-10 22:38:19 -0500

    work zip script

--- .config/zip.sh
+++ .config/zip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DROPBOX="$HOME/Library/CloudStorage/Dropbox"
+
+# https://superuser.com/questions/312301/unix-zip-directory-but-excluded-specific-subdirectories-and-everything-within-t
 rm -rf $DROPBOX/site.zip
 (
 	cd "$HOME/.local/share/nvim" && zip -r -q "$DROPBOX/site.zip" site \
