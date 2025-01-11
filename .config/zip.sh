2e148d298047106437a02405a30c976c0881ac4a
jshu <jasonshu@terpmail.umd.edu>
2025-01-10 22:17:00 -0500

    work zip script

--- .config/zip.sh
+++ .config/zip.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 DROPBOX="$HOME/Library/CloudStorage/Dropbox"
-
 rm -rf $DROPBOX/site.zip
-cp -rf $HOME/.local/share/nvim/site /tmp/site
-
-# treesitter
-rm -rf /tmp/site/pack/deps/opt/nvim-treesitter/parser/*
-
-zip -q -r $DROPBOX/site.zip /tmp/site
-rm -rf /tmp/site
+(
+	cd $HOME/.local/share/nvim && zip -r -q $DROPBOX/site.zip site \
+		-x '*/pack/deps/opt/nvim-treesitter/parser/*' \
+		-x '*/pack/deps/opt/blink.cmp/target/release/*'
+)
