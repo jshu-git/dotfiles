e678e81f90ae1f1c67d06722dec6cd3c7a9eb174
jshu <jasonshu@terpmail.umd.edu>
2025-01-17 13:51:27 -0500

    mini.icons

--- .config/nvim/lua/plugins/ui/mini-icons.lua
+++ .config/nvim/lua/plugins/ui/mini-icons.lua
@@ -1,14 +1,3 @@
 local icons = require('mini.icons')
-icons.setup({
-  filetype = {
-    minifiles = { glyph = '󰉋', hl = 'Directory' },
-    minipick = { glyph = '󰍉' },
-    ['minideps-confirm'] = { glyph = '󰄵' },
-    ['mininotify-history'] = { glyph = '󰋚' },
-    undotree = { glyph = '󰕌' },
-    grapple = { glyph = '󰐃' },
-    lspinfo = { glyph = '󰋽' },
-  },
-})
+icons.setup()
 icons.mock_nvim_web_devicons()
--- icons.tweak_lsp_kind()
