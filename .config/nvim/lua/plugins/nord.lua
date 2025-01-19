cb01bf23f47bb811dd0f71043fdd4ebb3f9880a0
jshu <jasonshu@terpmail.umd.edu>
2025-01-19 15:14:43 -0500

    return to nord

---
+++ .config/nvim/lua/plugins/nord.lua
@@ -0,0 +1,52 @@
+-- local defaults = {
+--   polar_night = {
+--     origin = '#2E3440', -- nord0
+--     bright = '#3B4252', -- nord1
+--     brighter = '#434C5E', -- nord2
+--     brightest = '#4C566A', -- nord3
+--     light = '#616E88', -- out of palette
+--   },
+--   snow_storm = {
+--     origin = '#D8DEE9', -- nord4
+--     brighter = '#E5E9F0', -- nord5
+--     brightest = '#ECEFF4', -- nord6
+--   },
+--   frost = {
+--     polar_water = '#8FBCBB', -- nord7
+--     ice = '#88C0D0', -- nord8
+--     artic_water = '#81A1C1', -- nord9
+--     artic_ocean = '#5E81AC', -- nord10
+--   },
+--   aurora = {
+--     red = '#BF616A', -- nord11
+--     orange = '#D08770', -- nord12
+--     yellow = '#EBCB8B', -- nord13
+--     green = '#A3BE8C', -- nord14
+--     purple = '#B48EAD', -- nord15
+--   },
+-- }
+
+require('nord').setup({
+  -- diff = { mode = 'fg' },
+  search = { theme = 'vscode' },
+  on_highlights = function(hl, colors)
+    -- sentiment
+    hl.MatchParen = { link = 'Underlined' }
+
+    -- cmp
+    hl.BlinkCmpLabel = { fg = colors.polar_night.light }
+    hl.BlinkCmpLabelMatch = { fg = colors.snow_storm.origin }
+
+    -- snacks
+    hl.SnacksPickerMatch = { bg = colors.frost.artic_ocean, fg = colors.snow_storm.origin }
+    hl.SnacksPickerDir = { fg = colors.polar_night.light }
+    hl.SnacksPickerFile = { link = 'SnacksPickerDir' }
+
+    -- mini
+    hl.MiniFilesTitleFocused = { link = 'Visual' }
+    hl.MiniDiffSignAdd = { link = 'DiagnosticOk' }
+    hl.MiniDiffSignChange = { link = 'DiagnosticWarn' }
+    hl.MiniDiffSignDelete = { link = 'DiagnosticError' }
+    hl.MiniIndentScopeSymbol = { link = 'NonText' }
+  end,
+})
