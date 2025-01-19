545e262e27b7f9e2a45837ddbd083ec580715cfe
jshu <jasonshu@terpmail.umd.edu>
2025-01-19 15:07:41 -0500

    return to nord

--- .config/nvim/lua/mini-deps.lua
+++ .config/nvim/lua/mini-deps.lua
@@ -36,9 +36,9 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 now(function()
-  add({ source = 'catppuccin/nvim', name = 'catppuccin' })
-  require('plugins.catppuccin')
-  vim.cmd.colorscheme('catppuccin')
+  add('gbprod/nord.nvim')
+  require('plugins.nord')
+  vim.cmd.colorscheme('nord')
 end)
 
 later(function()
