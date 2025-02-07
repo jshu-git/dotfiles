6289569c9f9d36e3a273a75940a7a31cebef30c6
jshu <jasonshu@terpmail.umd.edu>
2025-02-07 12:18:36 -0500

    tabnine

--- .config/nvim/lua/mini-deps.lua
+++ .config/nvim/lua/mini-deps.lua
@@ -43,22 +43,22 @@ end)
 
 later(function()
   -- work
+  add('codota/tabnine-nvim')
   add('ojroques/nvim-osc52')
   if vim.env.SSH_CLIENT ~= nil then
     require('plugins.nvim-osc52')
-  end
 
-  add('codota/tabnine-nvim')
-  require('tabnine').setup({
-    disable_auto_comment = true,
-    accept_keymap = '<Tab>',
-    dismiss_keymap = '<C-]>',
-    debounce_ms = 800,
-    suggestion_color = { gui = '#808080', cterm = 244 },
-    exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
-    log_file_path = nil, -- absolute path to Tabnine log file
-    ignore_certificate_errors = false,
-  })
+    require('tabnine').setup({
+      disable_auto_comment = true,
+      accept_keymap = '<Tab>',
+      dismiss_keymap = '<C-]>',
+      debounce_ms = 800,
+      suggestion_color = { gui = '#808080', cterm = 244 },
+      exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
+      log_file_path = nil, -- absolute path to Tabnine log file
+      ignore_certificate_errors = false,
+    })
+  end
 
   -- lsp
   add({
