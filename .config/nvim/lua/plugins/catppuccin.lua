657b2738f9825bdac1978ffa562a76602a85e1a7
jshu <jasonshu@terpmail.umd.edu>
2025-02-08 10:17:21 -0500

    catpp

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -13,52 +13,66 @@
 --   sapphire = '#74c7ec',
 --   blue = '#89b4fa',
 --   lavender = '#b4befe',
+
 --   text = '#cdd6f4',
 --   subtext1 = '#bac2de',
 --   subtext0 = '#a6adc8',
+
 --   overlay2 = '#9399b2',
 --   overlay1 = '#7f849c',
 --   overlay0 = '#6c7086',
+
 --   surface2 = '#585b70',
 --   surface1 = '#45475a',
 --   surface0 = '#313244',
+
 --   base = '#1e1e2e',
 --   mantle = '#181825',
 --   crust = '#11111b',
 -- }
 
+-- for transparent background to work with cmp pmenu
+vim.o.pumblend = 1
+
 require('catppuccin').setup({
   flavour = 'mocha',
+  transparent_background = true,
   show_end_of_buffer = true,
   no_bold = true,
   styles = {
     conditionals = {},
   },
-  custom_highlights = function(colors)
+  custom_highlights = function(c)
     return {
       VertSplit = { link = 'NonText' },
       WinSeparator = { link = 'VertSplit' },
       FloatBorder = { link = 'NonText' },
 
       -- cmp
-      BlinkCmpLabel = { fg = colors.overlay1 },
+      BlinkCmpLabel = { fg = c.overlay0 },
 
       -- mini
       MiniJump = { link = 'SpellRare' },
       MiniFilesTitleFocused = { link = 'Visual' },
 
       -- snacks
-      -- SnacksPickerDir = { fg = colors.surface2 },
+      -- SnacksPickerDir = { fg = c.surface2 },
       SnacksPickerFile = { link = 'SnacksPickerDir' },
-      -- SnacksPickerMatch = { bg = colors.overlay0, fg = colors.text },
+      -- SnacksPickerMatch = { bg = c.overlay0, fg = c.text },
       SnacksPickerMatch = { link = 'Search' },
-      SnacksPickerListCursorLine = { bg = colors.surface0 },
+      SnacksPickerListCursorLine = { bg = c.surface0 },
 
       -- sentiment
       MatchParen = { link = 'Underlined' },
 
       -- treesitter
       TreesitterContext = { link = 'ColorColumn' },
+
+      -- copilot
+      CopilotSuggestion = { fg = c.surface1 },
+      CopilotAnnotation = { link = 'Search' },
+      NeocodeiumSuggestion = { fg = c.surface1 },
+      NeocodeiumLabel = { link = 'Search' },
     }
   end,
   integrations = {
