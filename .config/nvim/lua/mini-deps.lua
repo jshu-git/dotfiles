fb0458afa72e10e1e53097ae6479c9d118bdad99
jshu <jasonshu@terpmail.umd.edu>
2025-02-09 14:39:41 -0500

    deps

--- .config/nvim/lua/mini-deps.lua
+++ .config/nvim/lua/mini-deps.lua
@@ -84,7 +84,6 @@ later(function()
   add('numToStr/Comment.nvim')
   add('nmac427/guess-indent.nvim')
   add('chrisgrieser/nvim-rip-substitute')
-  add('SunnyTamang/select-undo.nvim')
   add('stevearc/quicker.nvim')
   vim.cmd('runtime! lua/plugins/editing/*')
 
