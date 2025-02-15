a725cb68efb927c6553e4fe270709dbec7d1a302
jshu <jasonshu@terpmail.umd.edu>
2025-02-14 23:18:45 -0500

    binary

--- .config/nvim/colors/binary.lua
+++ .config/nvim/colors/binary.lua
@@ -4,10 +4,17 @@ local p = {
   bg = '#1b1e28',
   fg = '#cdd6f4',
 
-  red = '#cc6666',
-  green = '#b5bd68',
+  red = '#732626',
+  red_bg = '',
+  green = '#61662d',
+  green_bg = '',
   yellow = '#f0c674',
-  blue = '#81a2be',
+  yellow_bg = '',
+  cyan = '#30756f',
+  cyan_bg = '',
+  blue = '#365269',
+  blue_bg = '',
+  teal = '#5de4c7', -- poimandres
 
   bg10 = '#2d3242',
   bg20 = '#3f465d',
@@ -25,71 +32,80 @@ end
 
 local highlights = {
   -- builtin
-  CurSearch = { fg = p.black, bg = p.yellow },
   CursorLine = { bg = p.bg10 },
-  -- DiffAdd = { fg = nil, bg = p.green_bg },
-  -- DiffChange = { fg = nil, bg = p.cyan_bg },
-  -- DiffDelete = { fg = nil, bg = p.red_bg },
-  -- DiffText = { fg = nil, bg = p.yellow_bg },
   EndOfBuffer = { fg = p.bg20 },
   ErrorMsg = { fg = p.white, bg = p.red },
   FloatBorder = { fg = p.bg30 },
-  IncSearch = { link = 'CurSearch' },
   LineNr = { fg = p.bg20 },
   MatchParen = { link = 'Underlined' },
   -- NonText = { fg = p.bg10 },
-  Pmenu = { bg = p.bg10 },
-  PmenuSel = { bg = p.bg20 },
-  Search = { fg = p.white, bg = p.bg30 },
   StatusLine = { bg = p.bg10 },
   Visual = { bg = p.bg30 },
   WarningMsg = { fg = p.yellow, bg = nil },
   -- Whitespace = { fg = p.bg10 },
 
-  -- syntax
-  Comment = { fg = p.bg50, italic = true },
+  -- search
+  CurSearch = { fg = p.black, bg = p.yellow },
+  -- IncSearch = { link = 'CurSearch' },
+  Search = { fg = p.white, bg = p.bg30 },
 
-  -- Other community standard
+  -- pmenu
+  Pmenu = { bg = p.bg10 },
+  PmenuSel = { bg = p.bg20 },
+
+  -- syntax
+  Boolean = { fg = p.teal },
+  ['@boolean'] = { link = 'Boolean' },
+  Comment = { fg = p.bg50, italic = true },
+  ['@comment'] = { link = 'Comment' },
+  Number = { fg = p.teal },
+  ['@number'] = { link = 'Number' },
+  String = { fg = p.teal },
+  ['@string'] = { link = 'String' },
   Bold = { bold = true },
   Italic = { italic = true },
   Underlined = { underline = true },
 
-  -- -- Patch diff
-  -- diffAdded = { fg = p.green },
-  -- diffChanged = { fg = p.cyan },
-  -- diffFile = { fg = p.yellow },
-  -- diffLine = { fg = p.blue },
-  -- diffRemoved = { fg = p.red },
+  -- diff
+  DiffText = { bg = p.yellow },
+  DiffAdd = { bg = p.green },
+  DiffChange = { bg = p.cyan },
+  DiffDelete = { bg = p.red },
+  diffAdded = { fg = p.green },
+  diffChanged = { fg = p.cyan },
+  diffFile = { fg = p.yellow },
+  diffLine = { fg = p.blue },
+  diffRemoved = { fg = p.red },
   -- Added = { fg = p.green },
   -- Changed = { fg = p.cyan },
   -- Removed = { fg = p.red },
 
-  -- Built-in diagnostic
+  -- diag
   DiagnosticError = { fg = p.red },
-  DiagnosticHint = { fg = p.bg50 },
+  DiagnosticHint = { fg = p.cyan },
   DiagnosticInfo = { fg = p.blue },
   DiagnosticOk = { fg = p.green },
   DiagnosticWarn = { fg = p.yellow },
   DiagnosticUnderlineError = { undercurl = true },
-  DiagnosticUnderlineHint = { fg = p.bg50, undercurl = true },
+  DiagnosticUnderlineHint = { fg = p.cyan, undercurl = true },
   DiagnosticUnderlineInfo = { fg = p.blue, undercurl = true },
   DiagnosticUnderlineOk = { fg = p.green, undercurl = true },
   DiagnosticUnderlineWarn = { fg = p.yellow, undercurl = true },
-  DiagnosticFloatingError = { fg = p.red },
-  DiagnosticFloatingHint = { fg = p.bg50 },
-  DiagnosticFloatingInfo = { fg = p.blue },
-  DiagnosticFloatingOk = { fg = p.green },
-  DiagnosticFloatingWarn = { fg = p.yellow },
-  DiagnosticVirtualTextError = { link = 'DiagnosticError' },
-  DiagnosticVirtualTextWarn = { link = 'DiagnosticWarn' },
-  DiagnosticVirtualTextInfo = { link = 'DiagnosticInfo' },
-  DiagnosticVirtualTextHint = { link = 'DiagnosticHint' },
-  DiagnosticVirtualTextOk = { link = 'DiagnosticOk' },
-  DiagnosticSignError = { link = 'DiagnosticError' },
-  DiagnosticSignWarn = { link = 'DiagnosticWarn' },
-  DiagnosticSignInfo = { link = 'DiagnosticInfo' },
-  DiagnosticSignHint = { link = 'DiagnosticHint' },
-  DiagnosticSignOk = { link = 'DiagnosticOk' },
+  -- DiagnosticFloatingError = { link = 'DiagnosticError' },
+  -- DiagnosticFloatingHint = { link = 'DiagnosticHint' },
+  -- DiagnosticFloatingInfo = { link = 'DiagnosticInfo' },
+  -- DiagnosticFloatingOk = { link = 'DiagnosticOk' },
+  -- DiagnosticFloatingWarn = { link = 'DiagnosticWarn' },
+  -- DiagnosticVirtualTextError = { link = 'DiagnosticError' },
+  -- DiagnosticVirtualTextWarn = { link = 'DiagnosticWarn' },
+  -- DiagnosticVirtualTextInfo = { link = 'DiagnosticInfo' },
+  -- DiagnosticVirtualTextHint = { link = 'DiagnosticHint' },
+  -- DiagnosticVirtualTextOk = { link = 'DiagnosticOk' },
+  -- DiagnosticSignError = { link = 'DiagnosticError' },
+  -- DiagnosticSignWarn = { link = 'DiagnosticWarn' },
+  -- DiagnosticSignInfo = { link = 'DiagnosticInfo' },
+  -- DiagnosticSignHint = { link = 'DiagnosticHint' },
+  -- DiagnosticSignOk = { link = 'DiagnosticOk' },
 
   -- lsp
   LspSignatureActiveParameter = { bg = p.bg30 },
@@ -98,6 +114,7 @@ local highlights = {
   -- plugins
   -- blink
   BlinkCmpLabel = { fg = p.bg50 },
+  BlinkCmpLabelMatch = { fg = p.white },
 
   -- mini
   -- MiniJump = { reverse = true },
