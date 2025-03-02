74f47090c1b2793980ab5c038b02d4951b24705d
jshu <jasonshu@terpmail.umd.edu>
2025-03-02 17:01:53 -0500

    trailspace

--- .config/nvim/lua/plugins/ui/mini-trailspace.lua
+++ .config/nvim/lua/plugins/ui/mini-trailspace.lua
@@ -1,3 +1,3 @@
 local trailspace = require('mini.trailspace')
 trailspace.setup()
-vim.keymap.set('n', '<leader>tt', trailspace.trim, { desc = 'Trim Trailspace' })
+vim.keymap.set('n', '<leader>T', trailspace.trim, { desc = 'Trim Trailspace' })
