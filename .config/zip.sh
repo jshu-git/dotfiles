c226a1d8f9a13fee9ed96b92aa25265b67125396
jshu <jasonshu@terpmail.umd.edu>
2025-03-03 18:28:30 -0500

    zip

--- .config/zip.sh
+++ .config/zip.sh
@@ -4,8 +4,10 @@ DROPBOX="$HOME/Library/CloudStorage/Dropbox"
 
 # https://superuser.com/questions/312301/unix-zip-directory-but-excluded-specific-subdirectories-and-everything-within-t
 rm -rf $DROPBOX/site.zip
-(
-	cd "$HOME/.local/share/nvim" && zip -r -q "$DROPBOX/site.zip" site \
-		-x "*/nvim-treesitter/parser/**" \
-		-x "*/blink.cmp/target/release/**"
-)
+cd "$HOME/.local/share/nvim" && zip -r -q "$DROPBOX/site.zip" site
+
+# (
+# 	cd "$HOME/.local/share/nvim" && zip -r -q "$DROPBOX/site.zip" site \
+# 		-x "*/nvim-treesitter/parser/**" \
+# 		-x "*/blink.cmp/target/release/**"
+# )
