0d0cd957752ae9e24b9dabee86e582bbab8018e1
jshu <jasonshu@terpmail.umd.edu>
2025-03-25 19:55:40 -0400

    tiny-inline-diagnostic

--- .config/nvim/lua/plugins/lsp/tiny-inline-diagnostic.lua
+++ .config/nvim/lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -1,9 +1,6 @@
 vim.diagnostic.config({ virtual_text = false })
 require('tiny-inline-diagnostic').setup({
   preset = 'simple',
-  hi = {
-    mixing_color = '#1b1e28',
-  },
   options = {
     show_source = true,
     throttle = 0,
