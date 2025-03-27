87d0b381c50eed52a0aa2842b4fb1d4308625cf8
jshu <jasonshu@terpmail.umd.edu>
2025-03-26 20:05:01 -0400

    copilot

--- .config/nvim/lua/mini-deps.lua
+++ .config/nvim/lua/mini-deps.lua
@@ -53,16 +53,18 @@ later(function()
   add({ source = 'saghen/blink.cmp', checkout = 'v1.0.0' })
   add('windwp/nvim-autopairs')
   add('monkoose/neocodeium')
-  -- add('zbirenbaum/copilot.lua')
-  -- add({
-  --   source = 'CopilotC-Nvim/CopilotChat.nvim',
-  --   depends = {
-  --     'zbirenbaum/copilot.lua',
-  --     'nvim-lua/plenary.nvim',
-  --   },
-  -- })
-  -- require('plugins.copilot')
   vim.cmd('runtime! lua/plugins/cmp/*')
+
+  -- copilot
+  add('zbirenbaum/copilot.lua')
+  add({
+    source = 'CopilotC-Nvim/CopilotChat.nvim',
+    depends = {
+      'zbirenbaum/copilot.lua',
+      'nvim-lua/plenary.nvim',
+    },
+  })
+  -- require('plugins.copilot')
 
   -- ui
   add('utilyre/sentiment.nvim')
